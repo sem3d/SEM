4f277a5060383f1cee7322164b049cf7ee3bb453
Ludovic Aubry <ludovic.aubry@cea.fr>
2013-12-13 16:19:39 +0100

    [CLEANUP] whitespace, indent, unused variables

--- SEM3D/SRC/drive_sem.f90
+++ SEM3D/SRC/drive_sem.f90
@@ -391,5 +391,9 @@ end subroutine sem
 !! Local Variables:
 !! mode: f90
 !! show-trailing-whitespace: t
+!! f90-do-indent: 4
+!! f90-if-indent: 4
+!! f90-program-indent: 4
+!! f90-continuation-indent: 4
 !! End:
 !! vim: set sw=4 ts=8 et tw=80 smartindent : !!
