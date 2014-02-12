54a6efc4c4bc53ccb929e85b7d9cfcc873ea3575
Sébastien Terrana <sebastien.terrana@cea.fr>
2014-02-12 15:45:38 +0100

    New subroutine checking the mesh orientation and convexity.

--- SEM2D/SRC/check_inputs_and_mesh.F90
+++ SEM2D/SRC/check_inputs_and_mesh.F90
@@ -9,7 +9,6 @@
 !!
 !<
 
-
 subroutine check_inputs_and_mesh(Tdomain)
     use sdomain    
     use constants
@@ -46,5 +45,57 @@ subroutine check_inputs_and_mesh(Tdomain)
         (Tdomain%type_elem.NE.GALERKIN_DG_WEAK)) then
         STOP "This choice for elem_type does not exist. Choose : continuous, dg-strong, or dg-weak"
     endif
+
+    ! Check the regularity of the mesh :
+    call check_mesh(Tdomain)
+
+end subroutine check_inputs_and_mesh
+
+
+!>
+!!\brief Subroutine check_inputs_and_mesh() checks the right orientation
+!! of each element of the mesh and its convexity.
+!! and are already implemented. If not, the program terminates.
+!!\author S. TERRANA
+!!\version 1.0
+!!\date 10/02/2014
+!!
+!<
+subroutine check_inputs_and_mesh(Tdomain)
+    use sdomain    
+    use constants
+    implicit none
+    
+    type(domain), intent(inout)  :: Tdomain
+    real, dimension(0:1)         :: vect1, vect2, vect3
+    integer, dimension (0:3)     :: Vertices
+    integer :: n, nv0, nv1, nv2, nv3
+    real    :: crossp1, crossp2
+    
+    
+    do n=0,Tdomain%n_elem-1
+        Vertices = Tdomain%specel(n)%Near_Vertex
+        nv0 = Tdomain%sVertex(Vertices(0))%Glob_Numbering
+        nv1 = Tdomain%sVertex(Vertices(1))%Glob_Numbering
+        nv2 = Tdomain%sVertex(Vertices(2))%Glob_Numbering
+        nv3 = Tdomain%sVertex(Vertices(3))%Glob_Numbering
+
+        vect1(0) = Tdomain%coord_nodes(0,nv1) - Tdomain%coord_nodes(0,nv0)
+        vect1(1) = Tdomain%coord_nodes(1,nv1) - Tdomain%coord_nodes(1,nv0)
+        vect2(0) = Tdomain%coord_nodes(0,nv2) - Tdomain%coord_nodes(0,nv1)
+        vect2(1) = Tdomain%coord_nodes(1,nv2) - Tdomain%coord_nodes(1,nv1)
+        vect3(0) = Tdomain%coord_nodes(0,nv3) - Tdomain%coord_nodes(0,nv2)
+        vect3(1) = Tdomain%coord_nodes(1,nv3) - Tdomain%coord_nodes(1,nv2)
+        
+        crossp1 = Vect1(0)*Vect2(1) - Vect1(1)*Vect2(0)
+        crossp2 = Vect2(0)*Vect3(1) - Vect2(1)*Vect3(0)
+
+        if (crossp1 == 0. .OR. crossp2 == 0.) then
+            STOP "ERROR IN MESH : Element number ",n," is flat"
+        endif
+        if (crossp1 .LT. 0. .OR. crossp2 .LT. 0.) then
+            STOP "ERROR IN MESH : Element number ",n," is not properly oriented."
+        endif
+    enddo
 
 end subroutine check_inputs_and_mesh
