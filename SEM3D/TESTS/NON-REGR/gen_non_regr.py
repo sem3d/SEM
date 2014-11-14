e64adfc7d8e127dae65f2f586623f699796ef154
Ludovic Aubry <ludovic.aubry@cea.fr>
2014-11-14 15:34:55 +0100

    maj gen_non_regr

--- SEM3D/TESTS/NON-REGR/gen_non_regr.py
+++ SEM3D/TESTS/NON-REGR/gen_non_regr.py
@@ -3,18 +3,24 @@
 
 import sys
 import os
+import os.path as osp
 
 template = sys.argv[1]
 outputdir = sys.argv[2]
 
 MATERIAL = [
-    ("S", 6300., 2500., 2800., 5, 0., 0.), # S/F Vp Vs Rho ngll Qp Qs
-    ("F", 1300.,    0., 1000., 5, 0., 0.),
+    ("S", 6300., 2500., 2800., 5, 1000., 1000.), # S/F Vp Vs Rho ngll Qp Qs
+    ("F", 1300.,    0., 1000., 5, 1000., 1000.),
 ]
 
 MATDEF="%(t)s %(vp)s %(vs)s %(rho)s  %(ngll)s  %(ngll)s  %(ngll)s 0.1 %(qp)s %(qs)s"
 TXTLAYER="Epaisseur %(laythick)s m %(ndiv)s mailles"
-LAYER="%(laythick)s %(ndiv)s # Layer $(laynum)s"
+LAYER="%(laythick)s %(ndiv)s # Layer %(laynum)s"
+
+MAT_DAT_TEMPLATE = open(osp.join(template,"mat.dat")).read()
+MATER_IN_TEMPLATE = open(osp.join(template,"mater.in")).read()
+INPUT_SPEC_TEMPLATE = open(osp.join(template,"input.spec")).read()
+CAPTEURS_DAT_TEMPLATE = open(osp.join(template,"capteurs.dat")).read()
 
 subst = {
     "name" : "Test_name",
@@ -22,6 +28,7 @@ subst = {
     "snap_interval" : 0.01,
     "prorep" : "false",
     "restart_iter" : 0,
+    "sim_time" : 0.5,
 
 # Nb procs
     "nprocs" : 1,
@@ -60,6 +67,8 @@ subst = {
     "zs"   : 25.,
     "stype" : "impulse",
     "sfunc" : "ricker",
+# Attenuation
+    "nsolids" : 0,
 }
 
 
@@ -80,19 +89,115 @@ def gen_files(template, src, dest, subst):
 
 
 GENLAYERS = [
-    ("S__p%03(nprocs)d_g%(nglls)s", [ (600, 12, MATERIAL[0]) ]),
-    ("F__p%03(nprocs)d_g%(nglls)s", [ (600, 12, MATERIAL[1]) ]),
-    ("SF_p%03(nprocs)d_g%(nglls)s", [ (300,  6, MATERIAL[0]), (300, 6, MATERIAL[1]) ]),
+    ("Solid/noATN/noPML/g%(nglls)s/case", { "layers" : [ (600, 12, MATERIAL[0]) ],
+                                    "subst" : {"pmla" : 0, "nsolids":0},   } ),
+    ("Fluid/noATN/noPML/g%(nglls)s/case", { "layers": [ (600, 12, MATERIAL[1]) ],
+                                    "subst" : {"pmla" : 0, "nsolids":0},}),
+    ("SolidFluid/noATN/noPML/g%(nglls)s/case", { "layers": [ (300,  6, MATERIAL[0]), (300, 6, MATERIAL[1]) ],
+                                    "subst" : {"pmla" : 0, "nsolids":0},}),
+
+    ("Solid/noATN/PML/g%(nglls)s/case", { "layers" : [ (600, 12, MATERIAL[0]) ],
+                                  "subst" : {"pmla" : 1, "nsolids":0},   } ),
+    ("Fluid/noATN/PML/g%(nglls)s/case", { "layers": [ (600, 12, MATERIAL[1]) ],
+                                  "subst" : {"pmla" : 1, "nsolids":0},}),
+    ("SolidFluid/noATN/PML/g%(nglls)s/case", { "layers": [ (300,  6, MATERIAL[0]), (300, 6, MATERIAL[1]) ],
+                                  "subst" : {"pmla" : 1, "nsolids":0},}),
+
+    ("Solid/ATN1/noPML/g%(nglls)s/case", { "layers" : [ (600, 12, MATERIAL[0]) ],
+                                    "subst" : {"pmla" : 0, "nsolids":1},   } ),
+    ("SolidFluid/ATN1/noPML/g%(nglls)s/case", { "layers": [ (300,  6, MATERIAL[0]), (300, 6, MATERIAL[1]) ],
+                                    "subst" : {"pmla" : 0, "nsolids":1},}),
+
+    ("Solid/ATN1/PML/g%(nglls)s/case", { "layers" : [ (600, 12, MATERIAL[0]) ],
+                                 "subst" : {"pmla" : 1, "nsolids":1},   } ),
+    ("SolidFluid/ATN1/PML/g%(nglls)s/case", { "layers": [ (300,  6, MATERIAL[0]), (300, 6, MATERIAL[1]) ],
+                                 "subst" : {"pmla" : 1, "nsolids":1},}),
+
+    ("Solid/ATN3/noPML/g%(nglls)s/case", { "layers" : [ (600, 12, MATERIAL[0]) ],
+                                    "subst" : {"pmla" : 0, "nsolids":3},   } ),
+    ("SolidFluid/ATN3/noPML/g%(nglls)s/case", { "layers": [ (300,  6, MATERIAL[0]), (300, 6, MATERIAL[1]) ],
+                                    "subst" : {"pmla" : 0, "nsolids":3},}),
+
+    ("Solid/ATN3/PML/g%(nglls)s/case", { "layers" : [ (600, 12, MATERIAL[0]) ],
+                                 "subst" : {"pmla" : 1, "nsolids":3},   } ),
+    ("SolidFluid/ATN3/PML/g%(nglls)s/case", { "layers": [ (300,  6, MATERIAL[0]), (300, 6, MATERIAL[1]) ],
+                                 "subst" : {"pmla" : 1, "nsolids":3},}),
 ]
 
-GENPROCS = [ 1, 2, 4 ]
 
-GENGLL = [ 5, 6, 7 ]
+class SemCase(object):
+    def __init__(self, casename, template, casespec):
+        self.casename = casename
+        self.template = template.copy()
+        self.parse_casespec(casespec)
+
+    def parse_casespec(self, casespec):
+        self.compute_layers(casespec)
+        self.compute_material(casespec)
+
+    def compute_layers(self, casespec):
+        layers = []
+        for k, layer_def in enumerate(casespec):
+            dct = { "laythick" : layer_def[0], "ndiv": layer_def[1], "laynum":k }
+            layers.append( LAYER % dct )
+        self.template["nblayers"] = len(layers)
+        self.template["layers"] = "\n".join(layers)
+
+    def compute_material(self, casespec):
+        layers = []
+        for k, layer_def in enumerate(casespec):
+            ltype, vp, vs, rho, ngll, qp, qs = layer_def[2]
+            dct = { "t" : ltype,
+                    "vp" : vp,
+                    "vs":vs,
+                    "rho":rho,
+                    "ngll" : ngll,
+                    "qp" : qp,
+                    "qs" : qs,
+                    }
+            layers.append( MATDEF % dct )
+        self.template["nmat"] = len(layers)
+        self.template["matdef"] = "\n".join(layers)
+        
+    def gen_case_dir(self, destdir):
+        dirname = osp.join(destdir, self.casename)
+        try:
+            os.makedirs(dirname)
+        except OSError, err:
+            if err.errno!=17:
+                raise
+        self.gen_mater_in(dirname)
+        self.gen_mat_dat(dirname)
+        self.gen_input_spec(dirname)
+        self.gen_capteurs_dat(dirname)
 
 
-for title, layers in GENLAYERS:
-    layers, txtlayers = build_layers(layers)
-    for nproc in GENPROCS:
-        for ngll in GENGLL:
-            gendict = subst.copy()
-            
+    def gen_mater_in(self, dirname):
+        dest = open(osp.join(dirname, "mater.in"), "w")
+        dest.write(MATER_IN_TEMPLATE % self.template)
+        
+
+    def gen_mat_dat(self, dirname):
+        dest = open(osp.join(dirname, "mat.dat"), "w")
+        dest.write(MAT_DAT_TEMPLATE % self.template)
+
+    def gen_input_spec(self, dirname):
+        dest = open(osp.join(dirname, "input.spec"), "w")
+        dest.write(INPUT_SPEC_TEMPLATE % self.template)
+
+
+    def gen_capteurs_dat(self, dirname):
+        dest = open(osp.join(dirname, "capteurs.dat"), "w")
+        dest.write(CAPTEURS_DAT_TEMPLATE % self.template)
+
+
+
+def main():
+    subst["nglls"] = 5
+    for testname, testdesc in GENLAYERS:
+        name = testname % subst
+        case = SemCase(name, subst, testdesc["layers"])
+        case.gen_case_dir(outputdir)
+
+if __name__ == "__main__":
+    main()
