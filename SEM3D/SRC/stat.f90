1a4c8a77820726a8c99ec5081cdc02a7eddfbdd1
Franck HOUSSEN <houssen@ipgp.fr>
2015-05-21 17:01:22 +0200

    stat.f90 : gather all stats in one file instead of N

--- SEM3D/SRC/stat.f90
+++ SEM3D/SRC/stat.f90
@@ -33,27 +33,46 @@ module stat
     subroutine stat_finalize()
         use mpi
         implicit none
-        integer :: ierr, rank
-        character(len=5) :: rankName
-
-        call MPI_Comm_Rank (MPI_COMM_WORLD, rank, ierr)
-        write (rankName, '(I5)') rank
-        open (123, file="stat."//trim(adjustl(rankName))//".log", status="replace", action="write")
-
-        write (123,'(a,i4,a,f10.3,a,f10.3,a,f10.3,a,f10.3,a)') "TIMING - stat comm : rank ", rank, &
-        ", comm time ", giveTime+waitTime+takeTime,                                                  &
-        " sec [give ", giveTime, " sec, wait ", waitTime, " sec, take ", takeTime, " sec]"
-
-        write (123,'(a,i4,a,f10.3,a,f10.3,a,f10.3,a)') "TIMING - stat calc : rank ", rank,         &
-        ", calc time ", fintTime+fextTime, " sec [fint ", fintTime, " sec, fext ", fextTime, " sec]"
+        integer :: ierr, rank, sz, r
+        integer status(MPI_STATUS_SIZE)
 
         call system_clock(count=stopFullTick)
         deltaTick = stopFullTick-startFullTick
         if (deltaTick < 0) deltaTick = deltaTick+maxPeriod
         fullTime = real(deltaTick)/clockRate
-        write (123,'(a,i4,a,f10.3,a)') "TIMING - stat full : rank ", rank, ", full time ", fullTime, " sec"
 
-        close (123)
+        call MPI_Comm_Rank (MPI_COMM_WORLD, rank, ierr)
+        if (rank .gt. 0) then
+            call MPI_SEND(giveTime,1,MPI_DOUBLE_PRECISION,0,0,MPI_COMM_WORLD,ierr)
+            call MPI_SEND(waitTime,1,MPI_DOUBLE_PRECISION,0,0,MPI_COMM_WORLD,ierr)
+            call MPI_SEND(takeTime,1,MPI_DOUBLE_PRECISION,0,0,MPI_COMM_WORLD,ierr)
+            call MPI_SEND(fintTime,1,MPI_DOUBLE_PRECISION,0,0,MPI_COMM_WORLD,ierr)
+            call MPI_SEND(fextTime,1,MPI_DOUBLE_PRECISION,0,0,MPI_COMM_WORLD,ierr)
+            call MPI_SEND(fullTime,1,MPI_DOUBLE_PRECISION,0,0,MPI_COMM_WORLD,ierr)
+        else
+            open (123, file="stat.log", status="replace", action="write")
+            call MPI_Comm_Size (MPI_COMM_WORLD,   sz, ierr)
+            do r = 0, sz-1
+                if (r .gt. 0) then
+                    call MPI_RECV(giveTime,1,MPI_DOUBLE_PRECISION,r,0,MPI_COMM_WORLD,status,ierr)
+                    call MPI_RECV(waitTime,1,MPI_DOUBLE_PRECISION,r,0,MPI_COMM_WORLD,status,ierr)
+                    call MPI_RECV(takeTime,1,MPI_DOUBLE_PRECISION,r,0,MPI_COMM_WORLD,status,ierr)
+                    call MPI_RECV(fintTime,1,MPI_DOUBLE_PRECISION,r,0,MPI_COMM_WORLD,status,ierr)
+                    call MPI_RECV(fextTime,1,MPI_DOUBLE_PRECISION,r,0,MPI_COMM_WORLD,status,ierr)
+                    call MPI_RECV(fullTime,1,MPI_DOUBLE_PRECISION,r,0,MPI_COMM_WORLD,status,ierr)
+                end if
+
+                write (123,'(a,i4,a,f10.3,a,f10.3,a,f10.3,a,f10.3,a)') "TIMING - stat comm : rank ", r, &
+                ", comm time ", giveTime+waitTime+takeTime,                                             &
+                " sec [give ", giveTime, " sec, wait ", waitTime, " sec, take ", takeTime, " sec]"
+
+                write (123,'(a,i4,a,f10.3,a,f10.3,a,f10.3,a)') "TIMING - stat calc : rank ", r,         &
+                ", calc time ", fintTime+fextTime, " sec [fint ", fintTime, " sec, fext ", fextTime, " sec]"
+
+                write (123,'(a,i4,a,f10.3,a)') "TIMING - stat full : rank ", r, ", full time ", fullTime, " sec"
+            end do
+            close (123)
+        end if
     end subroutine stat_finalize
 
     subroutine stat_starttick()
