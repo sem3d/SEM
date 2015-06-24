037efbe16444967437198e1edbdc2130332b36ab
Franck HOUSSEN <houssen@ipgp.fr>
2015-06-24 11:59:14 +0200

    [SEM2D Q8] - mesh2dc : vi retab + vi reindent

--- MESH2D/SRC/main_mesh2d.cpp
+++ MESH2D/SRC/main_mesh2d.cpp
@@ -112,14 +112,14 @@ void Mesh2D::store_local_quad_points(MeshProcInfo& info, int qn, Quad& quad)
     // Create new local quad number
     info.m_quad_map[qn] = info.n_elements();
     for(int k=0;k<4;++k) {
-	int nn = quad.n[k];
-	int local_num = info.m_node_map[nn];
-	if (local_num==-1) {
-	    local_num = info.m_nodes.size()/2;
-	    info.m_nodes.push_back(m_px[nn]);
-	    info.m_nodes.push_back(m_py[nn]);
-	    info.m_node_map[nn] = local_num;
-	}
+        int nn = quad.n[k];
+        int local_num = info.m_node_map[nn];
+        if (local_num==-1) {
+            local_num = info.m_nodes.size()/2;
+            info.m_nodes.push_back(m_px[nn]);
+            info.m_nodes.push_back(m_py[nn]);
+            info.m_node_map[nn] = local_num;
+        }
     }
 }
 
@@ -136,25 +136,25 @@ void Mesh2D::prepare_new_proc_info(MeshProcInfo& info)
 void MeshProcInfo::get_quad_edge(Quad& q, int fn, int& v0, int& v1)
 {
     switch(fn) {
-    case 0:
-	v0 = q.n[0];
-	v1 = q.n[1];
-	break;
-    case 1:
-	v0 = q.n[1];
-	v1 = q.n[2];
-	break;
-    case 2:
-	v0 = q.n[3];
-	v1 = q.n[2];
-	break;
-    case 3:
-	v0 = q.n[0];
-	v1 = q.n[3];
-	break;
-    default:
-	printf("ERR: internal error in get_quad_edge\n");
-	exit(1);
+        case 0:
+            v0 = q.n[0];
+            v1 = q.n[1];
+            break;
+        case 1:
+            v0 = q.n[1];
+            v1 = q.n[2];
+            break;
+        case 2:
+            v0 = q.n[3];
+            v1 = q.n[2];
+            break;
+        case 3:
+            v0 = q.n[0];
+            v1 = q.n[3];
+            break;
+        default:
+            printf("ERR: internal error in get_quad_edge\n");
+            exit(1);
     }
     v0 = m_node_map[v0];
     v1 = m_node_map[v1];
@@ -180,26 +180,26 @@ void MeshProcInfo::add_edge_element(Quad& q, int fn, int en, int qn)
     int lqn = m_quad_map[qn];
     assert(m_edges_elems[2*en+1]==-1);
     if (m_edges_elems[2*en]==-1) {
-	j=0;
-	get_quad_edge(q, fn, v0, v1);
+        j=0;
+        get_quad_edge(q, fn, v0, v1);
     } else if (m_edges_elems[2*en+0]>lqn) {
-	j=0;
-	// Move elem0 to index 1
-	m_edges_elems[2*en+1] = m_edges_elems[2*en+0];
-	m_edges_local[2*en+1] = m_edges_local[2*en+0];
-	// Reset vertices for elem0
-	get_quad_edge(q, fn, v0, v1);
+        j=0;
+        // Move elem0 to index 1
+        m_edges_elems[2*en+1] = m_edges_elems[2*en+0];
+        m_edges_local[2*en+1] = m_edges_local[2*en+0];
+        // Reset vertices for elem0
+        get_quad_edge(q, fn, v0, v1);
     } else {
-	j=1;
+        j=1;
     }
     m_edges_elems[2*en+j] = lqn;
     m_edges_local[2*en+j] = fn;
     if (j==0) {
-	m_edges_vertices[2*en+0] = v0;
-	m_edges_vertices[2*en+1] = v1;
+        m_edges_vertices[2*en+0] = v0;
+        m_edges_vertices[2*en+1] = v1;
     } else {
-	v0 = m_edges_vertices[2*en+0];
-	v1 = m_edges_vertices[2*en+1];
+        v0 = m_edges_vertices[2*en+0];
+        v1 = m_edges_vertices[2*en+1];
     }
     //printf("qn=%d fn=%d en=%d : j=%d v0=%d v1=%d\n", qn, fn, en, j, v0, v1);
 }
@@ -208,61 +208,61 @@ void Mesh2D::gather_proc_info(MeshProcInfo& info, int rk)
 {
     prepare_new_proc_info(info);
     for(int qn=0;qn<m_quads.size();++qn) {
-	Quad& quad = m_quads[qn];
-	if (rk!=m_procs[qn]) {
-	    continue;
-	}
-	store_local_quad_points(info, qn, quad);
-	info.m_material.push_back(m_mat1[qn]);
-	info.m_material.push_back(0); // Flag solid/fluid... TODO remove
-	info.m_material.push_back(m_mat2[qn]);
-	for(int k=0;k<4;++k) {
-	    info.m_quads.push_back(info.m_node_map[quad.n[k]]);
-	    edge_idx_t e = make_edge_idx(quad.n[k], quad.n[(k+1)%4]);
-	    int& edge_num = info.m_edge_map[e]; //edge_num is offset by one so that zero means unassigned
-	    if (edge_num==0) {
-		edge_num = info.m_edge_map.size();
-		info.create_new_edge();
-		assert(edge_num==info.m_edges_local.size()/2);
-	    }
-	    info.add_edge_element(quad, k, edge_num-1, qn);
-	    info.m_edges.push_back(edge_num-1);
-	}
+        Quad& quad = m_quads[qn];
+        if (rk!=m_procs[qn]) {
+            continue;
+        }
+        store_local_quad_points(info, qn, quad);
+        info.m_material.push_back(m_mat1[qn]);
+        info.m_material.push_back(0); // Flag solid/fluid... TODO remove
+        info.m_material.push_back(m_mat2[qn]);
+        for(int k=0;k<4;++k) {
+            info.m_quads.push_back(info.m_node_map[quad.n[k]]);
+            edge_idx_t e = make_edge_idx(quad.n[k], quad.n[(k+1)%4]);
+            int& edge_num = info.m_edge_map[e]; //edge_num is offset by one so that zero means unassigned
+            if (edge_num==0) {
+                edge_num = info.m_edge_map.size();
+                info.create_new_edge();
+                assert(edge_num==info.m_edges_local.size()/2);
+            }
+            info.add_edge_element(quad, k, edge_num-1, qn);
+            info.m_edges.push_back(edge_num-1);
+        }
     }
     for(int k=0;k<info.m_edges_local.size();k+=2) assert(info.m_edges_local[k]!=-1);
     for(int qn=0;qn<m_quads.size();++qn) {
-	Quad& quad = m_quads[qn];
-	if (rk!=m_procs[qn]) {
-	    // mark edges and vertices involved in communications
-	    for(int k=0;k<4;++k) {
-		// Check vertices
-		int local_node_num = info.m_node_map[quad.n[k]];
-		if (local_node_num>=0) {
-		    Comm_proc& comm = info.m_comm[m_procs[qn]];
-		    comm.m_vertices_map[quad.n[k]] = local_node_num;
-		}
-		// Check edges
-		edge_idx_t e = make_edge_idx(quad.n[k], quad.n[(k+1)%4]);
-		int edge_num = info.m_edge_map[e]-1; //edge_num is offset by one so that zero means unassigned
-		if (edge_num>=0) {
-		    Comm_proc& comm = info.m_comm[m_procs[qn]];
-		    comm.m_edges_map[e]=edge_info_t(edge_num,1);
-		}
-	    }
-	}
+        Quad& quad = m_quads[qn];
+        if (rk!=m_procs[qn]) {
+            // mark edges and vertices involved in communications
+            for(int k=0;k<4;++k) {
+                // Check vertices
+                int local_node_num = info.m_node_map[quad.n[k]];
+                if (local_node_num>=0) {
+                    Comm_proc& comm = info.m_comm[m_procs[qn]];
+                    comm.m_vertices_map[quad.n[k]] = local_node_num;
+                }
+                // Check edges
+                edge_idx_t e = make_edge_idx(quad.n[k], quad.n[(k+1)%4]);
+                int edge_num = info.m_edge_map[e]-1; //edge_num is offset by one so that zero means unassigned
+                if (edge_num>=0) {
+                    Comm_proc& comm = info.m_comm[m_procs[qn]];
+                    comm.m_edges_map[e]=edge_info_t(edge_num,1);
+                }
+            }
+        }
     }
     map<int,Comm_proc>::iterator cit;
     for(cit=info.m_comm.begin();cit!=info.m_comm.end();++cit) {
-	Comm_proc& comm=cit->second;
-	map<edge_idx_t,edge_info_t>::const_iterator eit;
-	for(eit=comm.m_edges_map.begin();eit!=comm.m_edges_map.end();++eit) {
-	    comm.m_edges.push_back(eit->second.edge);
-	    comm.m_coherency.push_back(eit->second.coherency);
-	}
-	map<int,int>::const_iterator vit;
-	for(vit=comm.m_vertices_map.begin();vit!=comm.m_vertices_map.end();++vit) {
-	    comm.m_vertices.push_back(vit->second);
-	}
+        Comm_proc& comm=cit->second;
+        map<edge_idx_t,edge_info_t>::const_iterator eit;
+        for(eit=comm.m_edges_map.begin();eit!=comm.m_edges_map.end();++eit) {
+            comm.m_edges.push_back(eit->second.edge);
+            comm.m_coherency.push_back(eit->second.coherency);
+        }
+        map<int,int>::const_iterator vit;
+        for(vit=comm.m_vertices_map.begin();vit!=comm.m_vertices_map.end();++vit) {
+            comm.m_vertices.push_back(vit->second);
+        }
     }
 }
 
@@ -337,56 +337,56 @@ void read_dset_1d_i(hid_t g, const char* dname, vector<int>& v)
 
 void write_dset_1d_i(hid_t file_id, const char* dname, const vector<int>& v)
 {
-   hsize_t dims[1];
-   herr_t  status;
-   hid_t   dset_id;
-   hid_t   dspc_id;
+    hsize_t dims[1];
+    herr_t  status;
+    hid_t   dset_id;
+    hid_t   dspc_id;
 
-   if (H5Lexists(file_id, dname, H5P_DEFAULT)) {
-       dset_id = H5Dopen(file_id, dname, H5P_DEFAULT);
-       // TODO Check size coherency
+    if (H5Lexists(file_id, dname, H5P_DEFAULT)) {
+        dset_id = H5Dopen(file_id, dname, H5P_DEFAULT);
+        // TODO Check size coherency
     } else {
-	/* Create the data space for the dataset. */
-	dims[0] = v.size();
-	dspc_id = H5Screate_simple(1, dims, NULL);
-	dset_id = H5Dcreate2(file_id, dname, H5T_STD_I32LE, dspc_id,
-				      H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-	status = H5Sclose(dspc_id);
-   }
-   H5Dwrite(dset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &v[0]);
-   status = H5Dclose(dset_id);
+        /* Create the data space for the dataset. */
+        dims[0] = v.size();
+        dspc_id = H5Screate_simple(1, dims, NULL);
+        dset_id = H5Dcreate2(file_id, dname, H5T_STD_I32LE, dspc_id,
+                H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        status = H5Sclose(dspc_id);
+    }
+    H5Dwrite(dset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &v[0]);
+    status = H5Dclose(dset_id);
 }
 
 void write_dset_2d_i(hid_t file_id, const char* dname, int d2, const vector<int>& v)
 {
-   hsize_t     dims[2];
-   herr_t      status;
+    hsize_t     dims[2];
+    herr_t      status;
 
-   /* Create the data space for the dataset. */
-   dims[0] = v.size()/d2;
-   dims[1] = d2;
-   hid_t dataspace_id = H5Screate_simple(2, dims, NULL);
-   hid_t dataset_id = H5Dcreate2(file_id, dname, H5T_STD_I32LE, dataspace_id,
-				 H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-   H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &v[0]);
-   status = H5Dclose(dataset_id);
-   status = H5Sclose(dataspace_id);
+    /* Create the data space for the dataset. */
+    dims[0] = v.size()/d2;
+    dims[1] = d2;
+    hid_t dataspace_id = H5Screate_simple(2, dims, NULL);
+    hid_t dataset_id = H5Dcreate2(file_id, dname, H5T_STD_I32LE, dataspace_id,
+            H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &v[0]);
+    status = H5Dclose(dataset_id);
+    status = H5Sclose(dataspace_id);
 }
 
 void write_dset_2d_r(hid_t file_id, const char* dname, int d2, const vector<double>& v)
 {
-   hsize_t     dims[2];
-   herr_t      status;
+    hsize_t     dims[2];
+    herr_t      status;
 
-   /* Create the data space for the dataset. */
-   dims[0] = v.size()/d2;
-   dims[1] = d2;
-   hid_t dataspace_id = H5Screate_simple(2, dims, NULL);
-   hid_t dataset_id = H5Dcreate2(file_id, dname, H5T_IEEE_F64LE, dataspace_id,
-				 H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-   H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &v[0]);
-   status = H5Dclose(dataset_id);
-   status = H5Sclose(dataspace_id);
+    /* Create the data space for the dataset. */
+    dims[0] = v.size()/d2;
+    dims[1] = d2;
+    hid_t dataspace_id = H5Screate_simple(2, dims, NULL);
+    hid_t dataset_id = H5Dcreate2(file_id, dname, H5T_IEEE_F64LE, dataspace_id,
+            H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &v[0]);
+    status = H5Dclose(dataset_id);
+    status = H5Sclose(dataspace_id);
 }
 
 void read_quads(hid_t g, const char* dname, vector<Quad>& v)
@@ -421,7 +421,6 @@ void Mesh2D::read_mesh(const string& fname)
     m_procs.resize(m_quads.size(), 0);
     m_mat_max = 0;
     for(int k=0;k<m_mat1.size();++k) if (m_mat1[k]>m_mat_max) m_mat_max = m_mat1[k];
-
 }
 
 void Mesh2D::check_cell_orient()
@@ -450,8 +449,8 @@ void Mesh2D::partition_metis(int nproc)
     eptr.resize(ne+1);
     eind.resize(4*ne);
     for(int i=0;i<ne;++i) {
-	eptr[i] = 4*i;
-	for(int k=0;k<4;++k) eind[4*i+k] = m_quads[i].n[k];
+        eptr[i] = 4*i;
+        for(int k=0;k<4;++k) eind[4*i+k] = m_quads[i].n[k];
     }
     eptr[ne]=4*ne;
     METIS_MeshToDual(&ne, &nn, &eptr[0], &eind[0], &ncommon, &numflag, &xadj, &adjncy);
@@ -466,9 +465,9 @@ void Mesh2D::partition_metis(int nproc)
     real_t ubvec[1] = { 1.001 }; // load imbalance
     for(int k=0;k<nproc;++k) tpwgts[k] = 1./nproc;
     METIS_PartGraphRecursive(&ne, &ncon, xadj, adjncy,
-			     &vwgt[0], &vsize[0], &adjwgt[0], &inproc,
-			     &tpwgts[0], ubvec, options, &edgecut, &m_procs[0]);
-//    for(int k=0;k<ne;++k) printf("%d : %d\n", k, m_procs[k]);
+            &vwgt[0], &vsize[0], &adjwgt[0], &inproc,
+            &tpwgts[0], ubvec, options, &edgecut, &m_procs[0]);
+    //    for(int k=0;k<ne;++k) printf("%d : %d\n", k, m_procs[k]);
 }
 
 void Mesh2D::write_proc_field(const string& fname)
@@ -486,8 +485,8 @@ void Mesh2D::write_proc_file(const string& fname, int rk)
 {
     hid_t fid;
     if (access(fname.c_str(), F_OK)==0) {
-	// File already exists
-	unlink(fname.c_str());
+        // File already exists
+        unlink(fname.c_str());
     }
     fid = H5Fopen(fname.c_str(), H5F_ACC_RDWR|H5F_ACC_CREAT, H5P_DEFAULT);
 
@@ -517,17 +516,17 @@ void Mesh2D::write_proc_file(const string& fname, int rk)
     map<int,Comm_proc>::const_iterator it;
     int comm_count=0;
     for(it=info.m_comm.begin();it!=info.m_comm.end();++it) {
-	char grp_name[60];
-	snprintf(grp_name, 60, "Comm%05d", comm_count);
-	const Comm_proc& comm = it->second;
-	hid_t grp = H5Gcreate(fid, grp_name, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        char grp_name[60];
+        snprintf(grp_name, 60, "Comm%05d", comm_count);
+        const Comm_proc& comm = it->second;
+        hid_t grp = H5Gcreate(fid, grp_name, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
-	write_attr_int(grp, "processor", it->first);
-	write_dset_1d_i(grp, "vertices", comm.m_vertices);
-	write_dset_1d_i(grp, "edges", comm.m_edges);
-	write_dset_1d_i(grp, "coherency", comm.m_coherency);
-	H5Gclose(grp);
-	comm_count++;
+        write_attr_int(grp, "processor", it->first);
+        write_dset_1d_i(grp, "vertices", comm.m_vertices);
+        write_dset_1d_i(grp, "edges", comm.m_edges);
+        write_dset_1d_i(grp, "coherency", comm.m_coherency);
+        H5Gclose(grp);
+        comm_count++;
     }
 
     H5Fclose(fid);
@@ -537,8 +536,8 @@ int main(int argc, char** argv)
 {
     char fname[1024];
     if (argc<4) {
-	printf("Usage: mesh2dc Nproc mesh_input.h5 base_out\n");
-	exit(1);
+        printf("Usage: mesh2dc Nproc mesh_input.h5 base_out\n");
+        exit(1);
     }
 
     int nproc = atoi(argv[1]);
@@ -548,14 +547,14 @@ int main(int argc, char** argv)
 
     mesh.read_mesh(fmesh);
     if (nproc>1) {
-	mesh.partition_metis(nproc);
+        mesh.partition_metis(nproc);
     } else {
     }
     mesh.write_proc_field(fmesh);
 
     for(int k=0;k<nproc;++k) {
-	snprintf(fname, 1024,"%s.%04d.h5", argv[3], k);
-	mesh.write_proc_file(fname, k);
+        snprintf(fname, 1024,"%s.%04d.h5", argv[3], k);
+        mesh.write_proc_file(fname, k);
     }
 }
 // Local Variables:
