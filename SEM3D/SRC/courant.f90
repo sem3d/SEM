fc9b33a49610fa44dfc5dce2cc17f8de1b5e46f7
Filippo Gatti <filippo.gatti@centralesupelec.fr>
2015-06-30 18:31:14 +0200

    compute f_max (maximum frequency propagated)

--- SEM3D/SRC/courant.f90
+++ SEM3D/SRC/courant.f90
@@ -24,12 +24,19 @@ subroutine Compute_Courant (Tdomain,rg)
     integer :: i,j,k, n, ngllx,nglly,ngllz, idef0,idef1, mat, ierr
     real :: dx,dxmin,courant,courant_max
     real :: dt_min, dt, dt_loc
+    ! start modifs
+    real :: floc_max, dx_max, f_max
+    !end modifs
 
     courant = Tdomain%TimeD%courant
     courant_max = 0
     dt_loc = huge(1.)
+    floc_max  = huge(1.)
     do n = 0, Tdomain%n_elem -1
         dxmin = 1e10
+        ! start modifs
+        dx_max= 0
+        ! end modifs
 
         ngllx = Tdomain%specel(n)%ngllx
         nglly = Tdomain%specel(n)%nglly
@@ -45,38 +52,57 @@ subroutine Compute_Courant (Tdomain,rg)
                         (Tdomain%Globcoord(1,idef0) - Tdomain%Globcoord(1,idef1))**2 + &
                         (Tdomain%Globcoord(2,idef0) - Tdomain%Globcoord(2,idef1))**2
                     if(dx < dxmin) dxmin = dx
+                    ! start modifs
+                    if(dx > dx_max) dx_max = dx
+                    ! end modifs
                     idef1 = Tdomain%specel(n)%Iglobnum(i,j+1,k)
                     dx = (Tdomain%Globcoord(0,idef0) - Tdomain%Globcoord(0,idef1))**2 + &
                         (Tdomain%Globcoord(1,idef0) - Tdomain%Globcoord(1,idef1))**2 + &
                         (Tdomain%Globcoord(2,idef0) - Tdomain%Globcoord(2,idef1))**2
                     if(dx < dxmin) dxmin = dx
+                    ! start modifs
+                    if(dx > dx_max) dx_max = dx
+                    ! end modifs
                     idef1 = Tdomain%specel(n)%Iglobnum(i,j,k+1)
                     dx = (Tdomain%Globcoord(0,idef0) - Tdomain%Globcoord(0,idef1))**2 + &
                         (Tdomain%Globcoord(1,idef0) - Tdomain%Globcoord(1,idef1))**2 + &
                         (Tdomain%Globcoord(2,idef0) - Tdomain%Globcoord(2,idef1))**2
                     if(dx < dxmin) dxmin = dx
-
+                    ! start modifs
+                    if(dx > dx_max) dx_max = dx
+                    ! end modifs
                     idef1 = Tdomain%specel(n)%Iglobnum(i+1,j+1,k)
                     dx = (Tdomain%Globcoord(0,idef0) - Tdomain%Globcoord(0,idef1))**2 + &
                         (Tdomain%Globcoord(1,idef0) - Tdomain%Globcoord(1,idef1))**2 + &
                         (Tdomain%Globcoord(2,idef0) - Tdomain%Globcoord(2,idef1))**2
                     if(dx < dxmin) dxmin = dx
+                    ! start modifs
+                    if(dx > dx_max) dx_max = dx
+                    ! end modifs
                     idef1 = Tdomain%specel(n)%Iglobnum(i+1,j,k+1)
                     dx = (Tdomain%Globcoord(0,idef0) - Tdomain%Globcoord(0,idef1))**2 + &
                         (Tdomain%Globcoord(1,idef0) - Tdomain%Globcoord(1,idef1))**2 + &
                         (Tdomain%Globcoord(2,idef0) - Tdomain%Globcoord(2,idef1))**2
                     if(dx < dxmin) dxmin = dx
+                    ! start modifs
+                    if(dx > dx_max) dx_max = dx
+                    ! end modifs
                     idef1 = Tdomain%specel(n)%Iglobnum(i,j+1,k+1)
                     dx = (Tdomain%Globcoord(0,idef0) - Tdomain%Globcoord(0,idef1))**2 + &
                         (Tdomain%Globcoord(1,idef0) - Tdomain%Globcoord(1,idef1))**2 + &
                         (Tdomain%Globcoord(2,idef0) - Tdomain%Globcoord(2,idef1))**2
                     if(dx < dxmin) dxmin = dx
-
+                    ! start modifs
+                    if(dx > dx_max) dx_max = dx
+                    ! end modifs
                     idef1 = Tdomain%specel(n)%Iglobnum(i+1,j+1,k+1)
                     dx = (Tdomain%Globcoord(0,idef0) - Tdomain%Globcoord(0,idef1))**2 + &
                         (Tdomain%Globcoord(1,idef0) - Tdomain%Globcoord(1,idef1))**2 + &
                         (Tdomain%Globcoord(2,idef0) - Tdomain%Globcoord(2,idef1))**2
                     if(dx < dxmin) dxmin = dx
+                    ! start modifs
+                    if(dx > dx_max) dx_max = dx
+                    ! end modifs
                 enddo
             enddo
         enddo
@@ -86,11 +112,21 @@ subroutine Compute_Courant (Tdomain,rg)
         dt_loc = min(dt_loc, dxmin/Tdomain%sSubdomain(mat)%Pspeed)
         !!    courant = (Tdomain%sSubdomain(mat)%Pspeed * Tdomain%sSubdomain(mat)%Dt)/ dxmin
         !!    if (courant > courant_max) courant_max = courant
+
+        ! start modifs
+        dx_max = sqrt(dx_max)
+        floc_max  = min(floc_max,  Tdomain%sSubdomain(mat)%Sspeed/30/dx_max)
+        ! end modifs
+
     enddo
 
-    if(rg==0) &
-        write(*,*) 'dt_loc',dt_loc
+    if(rg==0) write(*,*) 'dt_loc',dt_loc
     call MPI_AllReduce (dt_loc, dt_min, 1, MPI_DOUBLE_PRECISION, MPI_MIN, Tdomain%communicateur, ierr)
+
+    ! start modifs
+    call MPI_AllReduce (floc_max, f_max, 1, MPI_DOUBLE_PRECISION, MPI_MIN, Tdomain%communicateur, ierr)
+    if(rg==0) write(*,*) 'f_max',f_max
+    ! end modifs
 
     dt = courant * dt_min
 
