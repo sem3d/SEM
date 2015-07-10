f3289208f83cdfffc550efb24e4675fefa4417bf
Filippo Gatti <filippo.gatti@centralesupelec.fr>
2015-07-10 11:47:41 +0200

    BUG_FIX

--- SEM3D/SRC/read_input.f90
+++ SEM3D/SRC/read_input.f90
@@ -539,7 +539,9 @@ contains
             Tdomain%Ssource(nsrc)%Zsource = src%coords(3)
             Tdomain%Ssource(nsrc)%i_type_source = src%type
             Tdomain%Ssource(nsrc)%amplitude_factor = src%amplitude
+            if (src%func .eq. 5) then
             Tdomain%Ssource(nsrc)%time_file = fromcstr(src%time_file)
+            end
             ! Comportement temporel
             Tdomain%Ssource(nsrc)%i_time_function = src%func
             Tdomain%Ssource(nsrc)%cutoff_freq = src%freq ! func=2,4
