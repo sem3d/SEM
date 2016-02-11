284cf02132871e201787225c82f8720923924b7a
Ludovic Aubry <ludovic.aubry@cea.fr>
2016-02-11 11:28:41 +0100

    ajout macro boucle elements dans index.h

--- index.h
+++ index.h
@@ -1,17 +1,28 @@
+#ifndef INDEX_H
+#define INDEX_H
+
 #if defined(SEM_VEC)
-#define     Density_(i,j,k,e)     m_Density(i,j,k,e)
-#define      Lambda_(i,j,k,e)      m_Lambda(i,j,k,e)
-#define          Mu_(i,j,k,e)          m_Mu(i,j,k,e)
-#define       Kappa_(i,j,k,e)       m_Kappa(i,j,k,e)
-#define       Cij_(m,i,j,k,e)       m_Cij(m,i,j,k,e)
-#define       Jacob_(i,j,k,e)       m_Jacob(i,j,k,e)
-#define InvGrad_(m,n,i,j,k,e) m_InvGrad(m,n,i,j,k,e)
+#define IND_IJKE(i,j,k,e)   e,i,j,k
+#define IND_DIJKE(m,i,j,k,e)   e,m,i,j,k
+#define IND_MNIJKE(m,n,i,j,k,e)   e,m,n,i,j,k
+
+#define BEGIN_ELEM_SUBLOOP(e,e0,e1) DO e = e0,e1
+#define END_ELEM_SUBLOOP  END DO
 #else
-#define     Density_(i,j,k,e)     m_Density(e,i,j,k)
-#define      Lambda_(i,j,k,e)      m_Lambda(e,i,j,k)
-#define          Mu_(i,j,k,e)          m_Mu(e,i,j,k)
-#define       Kappa_(i,j,k,e)       m_Kappa(e,i,j,k)
-#define       Cij_(m,i,j,k,e)         m_Cij(e,i,j,k,m)
-#define       Jacob_(i,j,k,e)       m_Jacob(e,i,j,k)
-#define InvGrad_(m,n,i,j,k,e)     m_InvGrad(e,i,j,k,n,m)
+#define IND_IJKE(i,j,k,e)   i,j,k,e
+#define IND_DIJKE(m,i,j,k,e)   m,i,j,k,e
+#define IND_MNIJKE(m,n,i,j,k,e)   m,n,i,j,k,e
+#define BEGIN_ELEM_SUBLOOP(e,e0,e1) e=e0
+#define END_ELEM_SUBLOOP
+#endif
+
+#define     Density_(i,j,k,e)     m_Density(IND_IJKE(i,j,k,e))
+#define      Lambda_(i,j,k,e)      m_Lambda(IND_IJKE(i,j,k,e))
+#define          Mu_(i,j,k,e)          m_Mu(IND_IJKE(i,j,k,e))
+#define       Kappa_(i,j,k,e)       m_Kappa(IND_IJKE(i,j,k,e))
+#define       Jacob_(i,j,k,e)       m_Jacob(IND_IJKE(i,j,k,e))
+#define       Cij_(m,i,j,k,e)      m_Cij(IND_DIJKE(m,i,j,k,e))
+#define InvGrad_(m,n,i,j,k,e) m_InvGrad(IND_MNIJKE(m,n,i,j,k,e))
+
+
 #endif
