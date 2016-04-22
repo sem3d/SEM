b2216b42139502daffa6456f06fe0009e1963148
Luciano De Carvalho <luciano.de-carvalho@ecp.fr>
2016-04-22 18:11:23 +0200

    random PML solid

--- SEM3D/SRC/build_prop_files.F90
+++ SEM3D/SRC/build_prop_files.F90
@@ -167,8 +167,15 @@ contains
 
         !write(*,*) " AFTER 6 rg = ", rg
 
+        !Adjusting PML values
+
+
         if(allocated(interpolatedRF)) deallocate(interpolatedRF)
         if(allocated(lambda))         deallocate(lambda)
+
+
+        write(*,*) "  Propagating properties to PML in rg ", rg
+        call propagate_PML_properties(Tdomain)
 
         !write(*,*) " AFTER 7 rg = ", rg
 
@@ -519,6 +526,320 @@ contains
         !end if
 
     end function
+
+    !---------------------------------------------------------------------------
+    !---------------------------------------------------------------------------
+    !---------------------------------------------------------------------------
+    !---------------------------------------------------------------------------
+    subroutine propagate_PML_properties(Tdomain)
+        !This subroutine modifies the "prop" table.
+        !In the PML elements it extrudes the properties according to the PML direction
+
+        implicit none
+        !INPUT
+        type(domain)    , intent(inout), target :: Tdomain
+
+        !LOCAL
+        integer :: ngll, lnum
+        integer :: iPoint
+        integer :: i, j, k, n !counters
+        integer :: LimPML1, LimPML2, LimPML3
+        integer :: dir, mat_index
+        double precision :: pointDens, pointLambda, pointMu;
+        logical :: verbose = .true.
+
+        !Propagating Properties over the PML
+
+        ! With the new PML definition, we can do better:
+        ! We can project x,y,z, on the pml plane, and interpolate the properties from the
+        ! projected coordinates
+
+        write(*,*) "Tdomain%not_PML_List = ", Tdomain%not_PML_List
+
+        do n = 0, Tdomain%n_elem-1
+            mat_index = Tdomain%specel(n)%mat_index
+
+            if(Tdomain%not_PML_List(mat_index)) cycle
+
+            ngll = Tdomain%sSubDomain(mat_index)%NGLL
+            lnum = Tdomain%specel(n)%lnum
+            !On the lower bound initialization
+            LimPML1 = 0
+            LimPML2 = 0
+            LimPML3 = 0
+            !To the upper bound transformation
+            if (Tdomain%sSubDomain(mat_index)%pml_width(0)<0) LimPML1 = ngll-1
+            if (Tdomain%sSubDomain(mat_index)%pml_width(1)<0) LimPML2 = ngll-1
+            if (Tdomain%sSubDomain(mat_index)%pml_width(2)<0) LimPML3 = ngll-1
+
+            dir = read_PML_Direction(Tdomain, mat_index)
+            if(verbose) write(*,*) "dir = ", dir
+            if(verbose) write(*,*) "Tdomain%specel(n)%domain = ", Tdomain%specel(n)%domain
+            pointDens = 0
+
+            !FOR TESTS
+            !select case (Tdomain%specel(n)%domain)
+            !    case (DM_SOLID_PML)
+            !        Tdomain%spmldom%Density_(:,:,:,lnum) = 0
+            !    case (DM_FLUID_PML)
+            !
+            !end select
+
+
+
+            select case(dir)
+                !Face X oriented
+                case(0)
+                    if(verbose) write(*,*) "Face X oriented"
+                    do j = 0, ngll-1
+                        do k = 0, ngll-1
+                            select case (Tdomain%specel(n)%domain)
+                                case (DM_SOLID_PML)
+                                    pointDens   = Tdomain%spmldom%Density_(LimPML1,j,k,lnum)
+                                    pointLambda = Tdomain%spmldom%Lambda_(LimPML1,j,k,lnum)
+                                    pointMu     = Tdomain%spmldom%Mu_(LimPML1,j,k,lnum)
+                                case (DM_FLUID_PML)
+
+                            end select
+
+                            do i = 0, ngll-1
+                                select case (Tdomain%specel(n)%domain)
+                                    case (DM_SOLID_PML)
+                                        Tdomain%spmldom%Density_(i,j,k,lnum) = pointDens
+                                        Tdomain%spmldom%Lambda_(i,j,k,lnum)  = pointLambda
+                                        Tdomain%spmldom%Mu_(i,j,k,lnum)      = pointMu
+                                    case (DM_FLUID_PML)
+
+                                end select
+                            end do
+                        enddo
+                    enddo
+                !
+                !Face Y oriented
+                case(1)
+                    if(verbose) write(*,*) "Face Y oriented"
+                    do i = 0, ngll-1
+                        do k = 0, ngll-1
+                            select case (Tdomain%specel(n)%domain)
+                                case (DM_SOLID_PML)
+                                    pointDens   = Tdomain%spmldom%Density_(i, LimPML2,k,lnum)
+                                    pointLambda = Tdomain%spmldom%Lambda_(i, LimPML2,k,lnum)
+                                    pointMu     = Tdomain%spmldom%Mu_(i, LimPML2,k,lnum)
+                                case (DM_FLUID_PML)
+
+                            end select
+
+                            do j = 0, ngll-1
+                                select case (Tdomain%specel(n)%domain)
+                                    case (DM_SOLID_PML)
+                                        Tdomain%spmldom%Density_(i,j,k,lnum) = pointDens
+                                        Tdomain%spmldom%Lambda_(i,j,k,lnum)  = pointLambda
+                                        Tdomain%spmldom%Mu_(i,j,k,lnum)      = pointMu
+                                    case (DM_FLUID_PML)
+
+                                end select
+                            end do
+                        enddo
+                    enddo
+
+                !Face Z oriented
+                case(2)
+                    if(verbose) write(*,*) "Face Z oriented"
+                    do i = 0, ngll-1
+                        do j = 0, ngll-1
+                            select case (Tdomain%specel(n)%domain)
+                                case (DM_SOLID_PML)
+                                    pointDens   = Tdomain%spmldom%Density_(i,j,LimPML3,lnum)
+                                    pointLambda = Tdomain%spmldom%Lambda_(i,j,LimPML3,lnum)
+                                    pointMu     = Tdomain%spmldom%Mu_(i,j,LimPML3,lnum)
+                                case (DM_FLUID_PML)
+
+                            end select
+
+                            do k = 0, ngll-1
+                                select case (Tdomain%specel(n)%domain)
+                                    case (DM_SOLID_PML)
+                                        Tdomain%spmldom%Density_(i,j,k,lnum) = pointDens
+                                        Tdomain%spmldom%Lambda_(i,j,k,lnum)  = pointLambda
+                                        Tdomain%spmldom%Mu_(i,j,k,lnum)      = pointMu
+                                    case (DM_FLUID_PML)
+
+                                end select
+                            end do
+                        enddo
+                    enddo
+
+                !Edge in XY
+                case(3)
+                    if(verbose) write(*,*) "Edge in XY"
+                    do k = 0, ngll-1
+                        select case (Tdomain%specel(n)%domain)
+                            case (DM_SOLID_PML)
+                                pointDens   = Tdomain%spmldom%Density_(LimPML1,LimPML2,k,lnum)
+                                pointLambda = Tdomain%spmldom%Lambda_(LimPML1,LimPML2,k,lnum)
+                                pointMu     = Tdomain%spmldom%Mu_(LimPML1,LimPML2,k,lnum)
+                            case (DM_FLUID_PML)
+
+                        end select
+
+                        do i = 0, ngll-1
+                            do j = 0, ngll-1
+                                select case (Tdomain%specel(n)%domain)
+                                    case (DM_SOLID_PML)
+                                        Tdomain%spmldom%Density_(i,j,k,lnum) = pointDens
+                                        Tdomain%spmldom%Lambda_(i,j,k,lnum)  = pointLambda
+                                        Tdomain%spmldom%Mu_(i,j,k,lnum)      = pointMu
+                                    case (DM_FLUID_PML)
+
+                                end select
+                            end do
+                        end do
+                    enddo
+
+                !Edge in YZ
+                case(4)
+                    if(verbose) write(*,*) "Edge in YZ"
+                    do i = 0, ngll-1
+                        select case (Tdomain%specel(n)%domain)
+                            case (DM_SOLID_PML)
+                                pointDens   = Tdomain%spmldom%Density_(i, LimPML2,LimPML3,lnum)
+                                pointLambda = Tdomain%spmldom%Lambda_(i, LimPML2,LimPML3,lnum)
+                                pointMu     = Tdomain%spmldom%Mu_(i, LimPML2,LimPML3,lnum)
+                            case (DM_FLUID_PML)
+
+                        end select
+
+                        do j = 0, ngll-1
+                            do k = 0, ngll-1
+                                select case (Tdomain%specel(n)%domain)
+                                    case (DM_SOLID_PML)
+                                        Tdomain%spmldom%Density_(i,j,k,lnum) = pointDens
+                                        Tdomain%spmldom%Lambda_(i,j,k,lnum)  = pointLambda
+                                        Tdomain%spmldom%Mu_(i,j,k,lnum)      = pointMu
+                                    case (DM_FLUID_PML)
+
+                                end select
+                            end do
+                        end do
+                    enddo
+
+                !Edge in ZX
+                case(5)
+                    if(verbose) write(*,*) "Edge in ZX"
+                    do j = 0, ngll-1
+                        select case (Tdomain%specel(n)%domain)
+                            case (DM_SOLID_PML)
+                                pointDens   = Tdomain%spmldom%Density_(LimPML1, j,LimPML3,lnum)
+                                pointLambda = Tdomain%spmldom%Lambda_(LimPML1, j,LimPML3,lnum)
+                                pointMu     = Tdomain%spmldom%Mu_(LimPML1, j,LimPML3,lnum)
+                            case (DM_FLUID_PML)
+
+                        end select
+
+                        do i = 0, ngll-1
+                            do k = 0, ngll-1
+                                select case (Tdomain%specel(n)%domain)
+                                    case (DM_SOLID_PML)
+                                        Tdomain%spmldom%Density_(i,j,k,lnum) = pointDens
+                                        Tdomain%spmldom%Lambda_(i,j,k,lnum)  = pointLambda
+                                        Tdomain%spmldom%Mu_(i,j,k,lnum)      = pointMu
+                                    case (DM_FLUID_PML)
+
+                                end select
+                            end do
+                        end do
+                    enddo
+
+                !Vertex
+                case(6)
+                    if(verbose) write(*,*) "Vertex"
+                    select case (Tdomain%specel(n)%domain)
+                        case (DM_SOLID_PML)
+                            pointDens   = Tdomain%spmldom%Density_(LimPML1, LimPML2,LimPML3,lnum)
+                            pointLambda = Tdomain%spmldom%Lambda_(LimPML1, LimPML2,LimPML3,lnum)
+                            pointMu     = Tdomain%spmldom%Mu_(LimPML1, LimPML2,LimPML3,lnum)
+                        case (DM_FLUID_PML)
+
+                    end select
+
+                    do i = 0, ngll-1
+                        do j = 0, ngll-1
+                            do k = 0, ngll-1
+                                select case (Tdomain%specel(n)%domain)
+                                    case (DM_SOLID_PML)
+                                        Tdomain%spmldom%Density_(i,j,k,lnum) = pointDens
+                                        Tdomain%spmldom%Lambda_(i,j,k,lnum)  = pointLambda
+                                        Tdomain%spmldom%Mu_(i,j,k,lnum)      = pointMu
+                                    case (DM_FLUID_PML)
+
+                                end select
+                            end do
+                        end do
+                    end do
+            end select
+        end do
+
+    end subroutine propagate_PML_properties
+
+    !---------------------------------------------------------------------------
+    !---------------------------------------------------------------------------
+    !---------------------------------------------------------------------------
+    !---------------------------------------------------------------------------
+    function read_PML_Direction(Tdomain, mat) result(dir)
+
+        implicit none
+        !INPUT
+        type(domain)    , intent(inout), target :: Tdomain
+        integer         , intent(in)            :: mat
+
+        !OUTPUT
+        integer :: dir
+
+        !LOCAL
+        integer :: error, code
+
+        real :: vx, vy, vz
+
+        !!! XXX: this wont work always, read comments from the call site
+        vx = Tdomain%sSubDomain(mat)%pml_width(0)
+        vy = Tdomain%sSubDomain(mat)%pml_width(1)
+        vz = Tdomain%sSubDomain(mat)%pml_width(2)
+
+        !/////////////Defining PML orientation
+        dir = 0
+        !Face X oriented
+        if  (vx /= 0 .and. vy == 0d0 .and. vz == 0d0) then
+            dir = 0
+        !Face Y oriented
+        elseif (vx == 0 .and. vy /= 0d0 .and. vz == 0d0) then
+            dir = 1
+        !Face Z oriented
+        elseif (vx == 0 .and. vy == 0d0 .and. vz /= 0d0) then
+            dir = 2
+        !Edge in XY
+        elseif (vx /= 0 .and. vy /= 0d0 .and. vz == 0d0) then
+            dir = 3
+
+        !Edge in YZ
+        elseif (vx == 0 .and. vy /= 0d0 .and. vz /= 0d0) then
+            dir = 4
+
+        !Edge in ZX
+        elseif (vx /= 0 .and. vy == 0d0 .and. vz /= 0d0) then
+            dir = 5
+
+        !Vertex in XYZ
+        elseif (vx /= 0 .and. vy /= 0d0 .and. vz /= 0d0) then
+            dir = 6
+
+        !Undefined PML
+        else
+            write(*,*) "ERROR in mat ", mat, " (PML) definition (directions), check 'material.input'"
+            call MPI_ABORT(Tdomain%communicateur, error, code)
+        end if
+
+    end function read_PML_Direction
+
 
 end module build_prop_files
                      
