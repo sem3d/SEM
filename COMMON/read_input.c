3d1ef8da0095e9d230c75ae3e497e790a471b414
Mandikizinoyou Eric Taro <eric.taro@centralesupelec.fr>
2016-05-20 14:02:49 +0200

    Changing error message material Multiple TO Random

--- COMMON/read_input.c
+++ COMMON/read_input.c
@@ -320,7 +320,7 @@ int expect_material_type(yyscan_t scanner, int* type) {
     if (cmp(scanner,"prem"))      { *type = 4; return 1; }
     if (cmp(scanner,"random"))    { *type = 5; return 1; }
 error:
-    msg_err(scanner, "Expected constant|gradient|earthchunk|prem|multiple");
+    msg_err(scanner, "Expected constant|gradient|earthchunk|prem|random");
     return 0;
 }
 
