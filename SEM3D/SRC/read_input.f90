51fd271c5525b933308a00a7af65ea52663607df
Franck HOUSSEN <houssen@ipgp.fr>
2016-09-16 18:16:12 +0200

    PML: pml_width = 0.

--- SEM3D/SRC/read_input.f90
+++ SEM3D/SRC/read_input.f90
@@ -164,6 +164,7 @@ contains
             Tdomain%sSubDomain(i)%dom = domain_from_type_char(material_type)
             Tdomain%sSubdomain(i)%material_definition = MATERIAL_CONSTANT
             Tdomain%sSubDomain(i)%deftype = MATDEF_VP_VS_RHO
+            Tdomain%sSubDomain(i)%pml_width = 0.
 
             ! call Lame_coefficients (Tdomain%sSubDomain(i)) ! XXX Make sure its done in definearrays
 
