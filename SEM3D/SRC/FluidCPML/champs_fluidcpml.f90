dbe72a1d6867645dea0b36c472def5dcc6331624
Franck HOUSSEN <houssen@ipgp.fr>
2016-11-04 16:24:41 +0100

    FluidCPML : MasUMat

--- SEM3D/SRC/FluidCPML/champs_fluidcpml.f90
+++ SEM3D/SRC/FluidCPML/champs_fluidcpml.f90
@@ -34,6 +34,8 @@ module champs_fluidpml
 
         ! Masse pour elements solide cpml
         real(fpp), dimension(:), allocatable :: DumpMat ! Delta 1st derivative term in (12a) from Ref1
+        real(fpp), dimension(:), allocatable :: MasUMat ! M^U <=> Delta term in (12a) from Ref1
+
         ! Element materials
         type(subdomain), dimension (:), pointer :: sSubDomain ! Point to Tdomain%sSubDomain
 
--- SEM3D/SRC/FluidCPML/dom_fluidcpml.F90
+++ SEM3D/SRC/FluidCPML/dom_fluidcpml.F90
@@ -50,10 +50,14 @@ contains
             dom%champs0%ForcesFl = 0d0
             dom%champs0%Phi = 0d0
             dom%champs0%VelPhi = 0d0
+
             ! Allocation de DumpMat pour les PML solides
             allocate(dom%DumpMat(0:dom%nglltot-1))
             dom%DumpMat = 0d0
 
+            ! Allocation de MasUMat pour les PML solides
+            allocate(dom%MasUMat(0:dom%nglltot-1))
+            dom%MasUMat = 0d0
         endif
 
         ! CPML parameters initialisation: for the very first implementation, parameters are hard-coded.
@@ -82,6 +86,8 @@ contains
         if(allocated(dom%champs1%VelPhi  )) deallocate(dom%champs1%VelPhi  )
 
         if(allocated(dom%DumpMat)) deallocate(dom%DumpMat)
+        if(allocated(dom%MasUMat)) deallocate(dom%MasUMat)
+
         call deallocate_dombase(dom)
     end subroutine deallocate_dom_fluidpml
 
