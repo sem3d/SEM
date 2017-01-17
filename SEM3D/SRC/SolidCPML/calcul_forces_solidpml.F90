13bd5ad120829dbd36de756e8e7c7498d014ef4a
Franck HOUSSEN <houssen@ipgp.fr>
2017-01-17 10:24:02 +0100

    compute_convolution_terms_3d

--- SEM3D/SRC/SolidCPML/calcul_forces_solidpml.F90
+++ SEM3D/SRC/SolidCPML/calcul_forces_solidpml.F90
@@ -359,34 +359,111 @@ contains
         real(fpp), intent(in), dimension(0:8) :: DUDV, DUDVn
         real(fpp), intent(out), dimension(0:20) :: LC
         !
-!        integer   :: dim0, r, i1, i2
-!        real(fpp) :: k0, d0, a0, dt
-!        real(fpp), dimension(0:5) :: b0, b1, b2, b3
-!        real(fpp), dimension(0:8) :: cf
-        !
-        stop 1
-        !
-!        LC(L120_DXX) = b0(kB120)*DUDV(DXX) + b1(kB120)*dom%R2_0(ee,DXX,i,j,k,bnum) + b2(kB120)*dom%R2_1(DXX,i,j,k,i1) + b3(kB120)*dom%R2_2(DXX,i,j,k,i2)
-!        LC(L2_DYY  ) = b0(kB2  )*DUDV(DYY) + b1(kB2  )*dom%R2_0(ee,DYY,i,j,k,bnum) + b2(kB2  )*dom%R2_1(DYY,i,j,k,i1) + b3(kB2  )*dom%R2_2(DYY,i,j,k,i2)
-!        LC(L1_DZZ  ) = b0(kB1  )*DUDV(DZZ) + b1(kB1  )*dom%R2_0(ee,DZZ,i,j,k,bnum) + b2(kB1  )*dom%R2_1(DZZ,i,j,k,i1) + b3(kB1  )*dom%R2_2(DZZ,i,j,k,i2)
-!        LC(L120_DXY) = b0(kB120)*DUDV(DXY) + b1(kB120)*dom%R2_0(ee,DXY,i,j,k,bnum) + b2(kB120)*dom%R2_1(DXY,i,j,k,i1) + b3(kB120)*dom%R2_2(DXY,i,j,k,i2)
-!        LC(L2_DYX  ) = b0(kB2  )*DUDV(DYX) + b1(kB2  )*dom%R2_0(ee,DYX,i,j,k,bnum) + b2(kB2  )*dom%R2_1(DYX,i,j,k,i1) + b3(kB2  )*dom%R2_2(DYX,i,j,k,i2)
-!        LC(L120_DXZ) = b0(kB120)*DUDV(DXZ) + b1(kB120)*dom%R2_0(ee,DXZ,i,j,k,bnum) + b2(kB120)*dom%R2_1(DXZ,i,j,k,i1) + b3(kB120)*dom%R2_2(DXZ,i,j,k,i2)
-!        LC(L1_DZX  ) = b0(kB1  )*DUDV(DZX) + b1(kB1  )*dom%R2_0(ee,DZX,i,j,k,bnum) + b2(kB1  )*dom%R2_1(DZX,i,j,k,i1) + b3(kB1  )*dom%R2_2(DZX,i,j,k,i2)
-!        LC(L021_DYX) = b0(kB021)*DUDV(DYX) + b1(kB021)*dom%R2_0(ee,DYX,i,j,k,bnum) + b2(kB021)*dom%R2_1(DYX,i,j,k,i1) + b3(kB021)*dom%R2_2(DYX,i,j,k,i2)
-!        LC(L2_DXY  ) = b0(kB2  )*DUDV(DXY) + b1(kB2  )*dom%R2_0(ee,DXY,i,j,k,bnum) + b2(kB2  )*dom%R2_1(DXY,i,j,k,i1) + b3(kB2  )*dom%R2_2(DXY,i,j,k,i2)
-!        LC(L2_DXX  ) = b0(kB2  )*DUDV(DXX) + b1(kB2  )*dom%R2_0(ee,DXX,i,j,k,bnum) + b2(kB2  )*dom%R2_1(DXX,i,j,k,i1) + b3(kB2  )*dom%R2_2(DXX,i,j,k,i2)
-!        LC(L021_DYY) = b0(kB021)*DUDV(DYY) + b1(kB021)*dom%R2_0(ee,DYY,i,j,k,bnum) + b2(kB021)*dom%R2_1(DYY,i,j,k,i1) + b3(kB021)*dom%R2_2(DYY,i,j,k,i2)
-!        LC(L0_DZZ  ) = b0(kB0  )*DUDV(DZZ) + b1(kB0  )*dom%R2_0(ee,DZZ,i,j,k,bnum) + b2(kB0  )*dom%R2_1(DZZ,i,j,k,i1) + b3(kB0  )*dom%R2_2(DZZ,i,j,k,i2)
-!        LC(L021_DYZ) = b0(kB021)*DUDV(DYZ) + b1(kB021)*dom%R2_0(ee,DYZ,i,j,k,bnum) + b2(kB021)*dom%R2_1(DYZ,i,j,k,i1) + b3(kB021)*dom%R2_2(DYZ,i,j,k,i2)
-!        LC(L0_DZY  ) = b0(kB0  )*DUDV(DYZ) + b1(kB0  )*dom%R2_0(ee,DYZ,i,j,k,bnum) + b2(kB0  )*dom%R2_1(DYZ,i,j,k,i1) + b3(kB0  )*dom%R2_2(DYZ,i,j,k,i2)
-!        LC(L012_DZX) = b0(kB012)*DUDV(DZX) + b1(kB012)*dom%R2_0(ee,DZX,i,j,k,bnum) + b2(kB012)*dom%R2_1(DZX,i,j,k,i1) + b3(kB012)*dom%R2_2(DZX,i,j,k,i2)
-!        LC(L1_DXZ  ) = b0(kB1  )*DUDV(DXZ) + b1(kB1  )*dom%R2_0(ee,DXZ,i,j,k,bnum) + b2(kB1  )*dom%R2_1(DXZ,i,j,k,i1) + b3(kB1  )*dom%R2_2(DXZ,i,j,k,i2)
-!        LC(L012_DZY) = b0(kB012)*DUDV(DZY) + b1(kB012)*dom%R2_0(ee,DZY,i,j,k,bnum) + b2(kB012)*dom%R2_1(DZY,i,j,k,i1) + b3(kB012)*dom%R2_2(DZY,i,j,k,i2)
-!        LC(L0_DYZ  ) = b0(kB0  )*DUDV(DYZ) + b1(kB0  )*dom%R2_0(ee,DYZ,i,j,k,bnum) + b2(kB0  )*dom%R2_1(DYZ,i,j,k,i1) + b3(kB0  )*dom%R2_2(DYZ,i,j,k,i2)
-!        LC(L1_DXX  ) = b0(kB1  )*DUDV(DXX) + b1(kB1  )*dom%R2_0(ee,DXX,i,j,k,bnum) + b2(kB1  )*dom%R2_1(DXX,i,j,k,i1) + b3(kB1  )*dom%R2_2(DXX,i,j,k,i2)
-!        LC(L0_DYY  ) = b0(kB0  )*DUDV(DYY) + b1(kB0  )*dom%R2_0(ee,DYY,i,j,k,bnum) + b2(kB0  )*dom%R2_1(DYY,i,j,k,i1) + b3(kB0  )*dom%R2_2(DYY,i,j,k,i2)
-!        LC(L012_DZZ) = b0(kB012)*DUDV(DZZ) + b1(kB012)*dom%R2_0(ee,DZZ,i,j,k,bnum) + b2(kB012)*dom%R2_1(DZZ,i,j,k,i1) + b3(kB012)*dom%R2_2(DZZ,i,j,k,i2)
+        integer   :: r, i1, i2
+        real(fpp) :: k0, d0, a0
+        real(fpp) :: k1, d1, a1
+        real(fpp) :: k2, d2, a2
+        real(fpp) :: dt
+        real(fpp) :: cf00, cf01, cf02, cf10, cf11, cf12, cf20, cf21, cf22
+        real(fpp) :: R0, R1, R2
+        real(fpp), dimension(0:5) :: b0, b1, b2, b3
+        real(fpp), dimension(0:2, 0:8) :: e
+        integer, dimension(0:8) :: sel
+
+        ! Initialize
+        k0 = dom%Kappa_0(ee,i,j,k,bnum)
+        a0 = dom%Alpha_0(ee,i,j,k,bnum)
+        d0 = dom%dxi_k_0(ee,i,j,k,bnum)
+        i1 = dom%I1(ee,bnum)
+        k1 = dom%Kappa_1(i,j,k,i1)
+        a1 = dom%Alpha_1(i,j,k,i1)
+        d1 = dom%dxi_k_1(i,j,k,i1)
+        i2 = dom%I2(ee,bnum)
+        k2 = dom%Kappa_2(i,j,k,i2)
+        a2 = dom%Alpha_2(i,j,k,i2)
+        d2 = dom%dxi_k_2(i,j,k,i2)
+        if (k0<0.or.k1<0.or.k2<0) then
+            write(*,*) "Erreur:", bnum, ee, i,j,k, k0
+            stop 1
+        endif
+        b0 = 1.
+        b1 = 0.
+        b2 = 0.
+        b3 = 0.
+        e = 0.
+
+        ! Li
+        call get_coefs_Li(dom, ee, bnum, i, j, k, b0, b1, e)
+
+        ! Lijk
+        e(0:2,kB012) = (/ a0, a1, a2+d2 /)
+        e(0:2,kB120) = (/ a1, a2, a0+d0 /)
+        e(0:2,kB021) = (/ a0, a2, a1+d1 /)
+        sel = CMP_ABC
+        call get_coefs_Lijk_3d(k1,k2,k0,a1,a2,a0,d1,d2,d0,b0(kB120),b1(kB120),b2(kB120),b3(kB120),sel(kB120))
+        call get_coefs_Lijk_3d(k0,k2,k1,a0,a2,a1,d0,d2,d1,b0(kB021),b1(kB021),b2(kB021),b3(kB021),sel(kB021))
+        call get_coefs_Lijk_3d(k0,k1,k2,a0,a1,a2,d0,d1,d2,b0(kB012),b1(kB012),b2(kB012),b3(kB012),sel(kB012))
+
+        ! Convolution term update
+        dt = dom%dt
+        do r=0,8
+            R0 = dom%R2_0(ee,r,i,j,k,bnum)
+            R1 = dom%R2_1(r,i,j,k,i1)
+            R2 = dom%R2_2(r,i,j,k,i2)
+            call cpml_compute_coefs(dom%cpml_integ, e(0,r), dt, cf00, cf01, cf02)
+            call cpml_compute_coefs(dom%cpml_integ, e(1,r), dt, cf10, cf11, cf12)
+            call cpml_compute_coefs(dom%cpml_integ, e(2,r), dt, cf20, cf21, cf22)
+            select case(sel(r))
+            case (CMP_ABC)
+                R0 = cf00*R0 + cf01*DUDVn(r) + cf02*DUDV(r)
+                R1 = cf10*R1 + cf11*DUDVn(r) + cf12*DUDV(r)
+                R2 = cf20*R2 + cf21*DUDVn(r) + cf22*DUDV(r)
+            case (CMP_AAC)
+                R0 = cf00*R0 + cf01*DUDVn(r) + cf02*DUDV(r)
+                R1 = cf10*R1 + cf11*R0         + cf12*R0
+                R2 = cf20*R2 + cf21*DUDVn(r) + cf22*DUDV(r)
+            case (CMP_ABA)
+                R0 = cf00*R0 + cf01*DUDVn(r) + cf02*DUDV(r)
+                R1 = cf10*R1 + cf11*DUDVn(r) + cf12*DUDV(r)
+                R2 = cf20*R2 + cf21*R0         + cf22*R0
+            case (CMP_ABB)
+                R0 = cf00*R0 + cf01*DUDVn(r) + cf02*DUDV(r)
+                R1 = cf10*R1 + cf11*DUDVn(r) + cf12*DUDV(r)
+                R2 = cf20*R2 + cf21*R1         + cf22*R1
+            case (CMP_AAA)
+                R0 = cf00*R0 + cf01*DUDVn(r) + cf02*DUDV(r)
+                R1 = cf10*R1 + cf11*R0         + cf12*R0
+                R2 = cf20*R2 + cf21*R1         + cf22*R1
+            case default
+                stop 1
+            end select
+            dom%R2_0(ee,r,i,j,k,bnum) = R0
+            dom%R2_1(r,i,j,k,i1) = R1
+            dom%R2_2(r,i,j,k,i2) = R2
+        end do
+
+        ! Convolve
+        LC(L120_DXX) = b0(kB120)*DUDV(DXX) + b1(kB120)*dom%R2_0(ee,DXX,i,j,k,bnum) + b2(kB120)*dom%R2_1(DXX,i,j,k,i1) + b3(kB120)*dom%R2_2(DXX,i,j,k,i2)
+        LC(L2_DYY  ) = b0(kB2  )*DUDV(DYY) + b1(kB2  )*dom%R2_0(ee,DYY,i,j,k,bnum) + b2(kB2  )*dom%R2_1(DYY,i,j,k,i1) + b3(kB2  )*dom%R2_2(DYY,i,j,k,i2)
+        LC(L1_DZZ  ) = b0(kB1  )*DUDV(DZZ) + b1(kB1  )*dom%R2_0(ee,DZZ,i,j,k,bnum) + b2(kB1  )*dom%R2_1(DZZ,i,j,k,i1) + b3(kB1  )*dom%R2_2(DZZ,i,j,k,i2)
+        LC(L120_DYX) = b0(kB120)*DUDV(DYX) + b1(kB120)*dom%R2_0(ee,DYX,i,j,k,bnum) + b2(kB120)*dom%R2_1(DYX,i,j,k,i1) + b3(kB120)*dom%R2_2(DYX,i,j,k,i2)
+        LC(L2_DYX  ) = b0(kB2  )*DUDV(DYX) + b1(kB2  )*dom%R2_0(ee,DYX,i,j,k,bnum) + b2(kB2  )*dom%R2_1(DYX,i,j,k,i1) + b3(kB2  )*dom%R2_2(DYX,i,j,k,i2)
+        LC(L120_DZX) = b0(kB120)*DUDV(DZX) + b1(kB120)*dom%R2_0(ee,DZX,i,j,k,bnum) + b2(kB120)*dom%R2_1(DZX,i,j,k,i1) + b3(kB120)*dom%R2_2(DZX,i,j,k,i2)
+        LC(L1_DZX  ) = b0(kB1  )*DUDV(DZX) + b1(kB1  )*dom%R2_0(ee,DZX,i,j,k,bnum) + b2(kB1  )*dom%R2_1(DZX,i,j,k,i1) + b3(kB1  )*dom%R2_2(DZX,i,j,k,i2)
+        LC(L021_DXY) = b0(kB021)*DUDV(DXY) + b1(kB021)*dom%R2_0(ee,DXY,i,j,k,bnum) + b2(kB021)*dom%R2_1(DXY,i,j,k,i1) + b3(kB021)*dom%R2_2(DXY,i,j,k,i2)
+        LC(L2_DXY  ) = b0(kB2  )*DUDV(DXY) + b1(kB2  )*dom%R2_0(ee,DXY,i,j,k,bnum) + b2(kB2  )*dom%R2_1(DXY,i,j,k,i1) + b3(kB2  )*dom%R2_2(DXY,i,j,k,i2)
+        LC(L2_DXX  ) = b0(kB2  )*DUDV(DXX) + b1(kB2  )*dom%R2_0(ee,DXX,i,j,k,bnum) + b2(kB2  )*dom%R2_1(DXX,i,j,k,i1) + b3(kB2  )*dom%R2_2(DXX,i,j,k,i2)
+        LC(L021_DYY) = b0(kB021)*DUDV(DYY) + b1(kB021)*dom%R2_0(ee,DYY,i,j,k,bnum) + b2(kB021)*dom%R2_1(DYY,i,j,k,i1) + b3(kB021)*dom%R2_2(DYY,i,j,k,i2)
+        LC(L0_DZZ  ) = b0(kB0  )*DUDV(DZZ) + b1(kB0  )*dom%R2_0(ee,DZZ,i,j,k,bnum) + b2(kB0  )*dom%R2_1(DZZ,i,j,k,i1) + b3(kB0  )*dom%R2_2(DZZ,i,j,k,i2)
+        LC(L021_DZY) = b0(kB021)*DUDV(DZY) + b1(kB021)*dom%R2_0(ee,DZY,i,j,k,bnum) + b2(kB021)*dom%R2_1(DZY,i,j,k,i1) + b3(kB021)*dom%R2_2(DZY,i,j,k,i2)
+        LC(L0_DZY  ) = b0(kB0  )*DUDV(DYZ) + b1(kB0  )*dom%R2_0(ee,DYZ,i,j,k,bnum) + b2(kB0  )*dom%R2_1(DYZ,i,j,k,i1) + b3(kB0  )*dom%R2_2(DYZ,i,j,k,i2)
+        LC(L012_DXZ) = b0(kB012)*DUDV(DXZ) + b1(kB012)*dom%R2_0(ee,DXZ,i,j,k,bnum) + b2(kB012)*dom%R2_1(DXZ,i,j,k,i1) + b3(kB012)*dom%R2_2(DXZ,i,j,k,i2)
+        LC(L1_DXZ  ) = b0(kB1  )*DUDV(DXZ) + b1(kB1  )*dom%R2_0(ee,DXZ,i,j,k,bnum) + b2(kB1  )*dom%R2_1(DXZ,i,j,k,i1) + b3(kB1  )*dom%R2_2(DXZ,i,j,k,i2)
+        LC(L012_DYZ) = b0(kB012)*DUDV(DYZ) + b1(kB012)*dom%R2_0(ee,DYZ,i,j,k,bnum) + b2(kB012)*dom%R2_1(DYZ,i,j,k,i1) + b3(kB012)*dom%R2_2(DYZ,i,j,k,i2)
+        LC(L0_DYZ  ) = b0(kB0  )*DUDV(DYZ) + b1(kB0  )*dom%R2_0(ee,DYZ,i,j,k,bnum) + b2(kB0  )*dom%R2_1(DYZ,i,j,k,i1) + b3(kB0  )*dom%R2_2(DYZ,i,j,k,i2)
+        LC(L1_DXX  ) = b0(kB1  )*DUDV(DXX) + b1(kB1  )*dom%R2_0(ee,DXX,i,j,k,bnum) + b2(kB1  )*dom%R2_1(DXX,i,j,k,i1) + b3(kB1  )*dom%R2_2(DXX,i,j,k,i2)
+        LC(L0_DYY  ) = b0(kB0  )*DUDV(DYY) + b1(kB0  )*dom%R2_0(ee,DYY,i,j,k,bnum) + b2(kB0  )*dom%R2_1(DYY,i,j,k,i1) + b3(kB0  )*dom%R2_2(DYY,i,j,k,i2)
+        LC(L012_DZZ) = b0(kB012)*DUDV(DZZ) + b1(kB012)*dom%R2_0(ee,DZZ,i,j,k,bnum) + b2(kB012)*dom%R2_1(DZZ,i,j,k,i1) + b3(kB012)*dom%R2_2(DZZ,i,j,k,i2)
     end subroutine compute_convolution_terms_3d
 
 #define NGLLVAL 4
