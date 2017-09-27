0109614c8a1e531829814ce7e6006a260b473309
Gael Burgos <gael.burgos@cea.fr>
2017-09-27 21:06:58 +0200

    regularize the mirror nodes output for coupling in the COMMON for the fluid.

--- COMMON/semdatafiles.F90
+++ COMMON/semdatafiles.F90
@@ -98,14 +98,23 @@ contains
         fname = pjoin(path_mirror,temp)
     end subroutine semname_mirrorfile_h5
 
-    subroutine semname_mirrorfile_nodes(rank, fname)
+    subroutine semname_mirrorfile_nodes_sl(rank, fname)
         implicit none
         integer, intent(in) :: rank
         character(Len=MAX_FILE_SIZE), intent(out) :: fname
         character(Len=MAX_FILE_SIZE) :: temp
-        write(temp,"(a7,I4.4,a10)") "mirror.",rank,".nodes.txt"
+        write(temp,"(a7,I4.4,a13)") "mirror.",rank,".nodes_sl.txt"
         fname = pjoin(path_mirror,temp)
-    end subroutine semname_mirrorfile_nodes
+    end subroutine semname_mirrorfile_nodes_sl
+
+    subroutine semname_mirrorfile_nodes_fl(rank, fname)
+        implicit none
+        integer, intent(in) :: rank
+        character(Len=MAX_FILE_SIZE), intent(out) :: fname
+        character(Len=MAX_FILE_SIZE) :: temp
+        write(temp,"(a7,I4.4,a13)") "mirror.",rank,".nodes_fl.txt"
+        fname = pjoin(path_mirror,temp)
+    end subroutine semname_mirrorfile_nodes_fl
 
     subroutine semname_read_capteurs(file,fnamef)
         implicit none
