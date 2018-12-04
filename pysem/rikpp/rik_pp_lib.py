3ab59ddb1528a0a91c9a2500328d61e7b3590fd4
Filippo Gatti <filippo.gatti@centralesupelec.fr>
2018-12-04 19:11:46 +0100

    [ADD] rikpp to convert/pp RIKs files

---
+++ pysem/rikpp/rik_pp_lib.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""
+Set of subroutines to post-process RIK output
+"""
+
+#=======================================================================
+# Required modules
+#=======================================================================
+import os 
+import sys
+import numpy as np
+import math
+import shutil
+import h5py
+from scipy import signal
+from scipy.interpolate import interp1d
+from INTFORTRAN import trapezoid
+import pylab as p
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+import matplotlib.lines as mlines
+import matplotlib.colors as mcolors
+from   matplotlib.patches import Polygon
+from   matplotlib.collections import PatchCollection
+from   matplotlib.colors import LinearSegmentedColormap, Normalize
+from   matplotlib.cm import ScalarMappable
+from   matplotlib import ticker
+from   matplotlib.colors import LogNorm
+from   matplotlib.image import NonUniformImage
+from   matplotlib.ticker import LogFormatter
+from   matplotlib.patches import Circle
+import seaborn as sns
+from   collections import OrderedDict 
+
+#=======================================================================
+# General informations
+#=======================================================================
+__author__ = "Filippo Gatti"
+__copyright__ = "Copyright 2018, MSSMat UMR CNRS 8579 - CentraleSupélec"
+__credits__ = ["Filippo Gatti"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Filippo Gatti"
+__email__ = "filippo.gatti@centralesupelec.fr"
+__status__ = "Beta"
+
+
+def get_rotation_tensor(aS,aD):
+    r'''From [N,E,D] et to [E,N,Z]
+    '''
+    MatMesh = np.zeros((3,3))
+    MatMesh[0,0] = -np.cos(aS)* np.cos(aD)
+    MatMesh[0,1] = +np.sin(aS)
+    MatMesh[1,0] = +np.sin(aS)* np.cos(aD)
+    MatMesh[1,1] = +np.cos(aS)
+    MatMesh[2,2] =  -1
+    for i in np.arange(3):
+        for j in np.arange(3):
+            if abs(MatMesh[i,j]) < 1e-15:
+                MatMesh[i,j] = 0.0
+    return MatMesh
+
+def moment_computation(M0,time,f,ts,gamma):
+    T 	   = 1.0/f
+    moment = np.zeros(len(time))
+    for i in np.arange(len(time)):
+        t = time[i]
+        if (t >= ts):
+            s = ((t-ts)/T)**gamma
+            moment[i] = M0* (1.0- (1.0+s)*math.e**(-s))
+    return moment
+
+def yeni_data_seti (data,dim,chunk, attrib,output):
+    ''' Attribuer un set de data de dimension dim
+    comme attribut de attrib dans un fichier output de hdf5 '''
+    dataset = output.create_dataset(attrib, dim, chunks=chunk)
+    dataset = data
+
+def vecteurs(aD,aS,aR,output):
+    ''' Calcul du vecteur normal Vnormal sur le plan de faille, et du vecteur unitaire
+    de glissement Vslip dans le repere de reference et les attribuer dans le fichier 
+    output de hdf5 '''
+    # En supposant que (puisque l'on multiplie avec MatMesh)
+    # x ----> EST
+    # y ----> NORD
+    # z ----> UP
+    Vnormal = np.array([+np.sin(aD)*np.cos(aS),\
+                        -np.sin(aD)*np.sin(aS),\
+                        +np.cos(aD)])
+    Vslip   = np.array([-np.sin(aR)*np.cos(aD)*np.cos(aS) + np.cos(aR)*np.sin(aS),\
+                        +np.sin(aR)*np.cos(aD)*np.sin(aS) + np.cos(aR)*np.cos(aS),\
+                        +np.sin(aR)*np.sin(aD)])
+    output.attrs['Vnormal'] = Vnormal
+    output.attrs['Vslip']   = Vslip
+    print "Vector normal to the fault : ", Vnormal
+    print "Vector of the slip         : ", Vslip
+    M = np.zeros((3,3))
+    for i in np.arange(3):
+        for j in np.arange(3):
+            M[i,j] = Vnormal[i]*Vslip[j]+Vnormal[j]*Vslip[i]
+    print 'Moment matrix of SEM3D code: '
+    print M[0,:]
+    print M[1,:]
+    print M[2,:]
+
+def read_moment_rate_RIK(dosya,dim):
+    ''' Lire la vitesse de moment par un fichier dosya et 
+    l'assigner a l'array de dimension dim '''
+    
+    Mrate = np.zeros((dim[0]*dim[1], dim[2]))
+    print 'Reading moment rate of all points...'
+    f = open (dosya, 'r')
+    # Pour tous les points
+    for i in np.arange(dim[0] * dim[1]):
+        # Histoire temporelle
+        for t in np.arange(dim[2]):
+            string = f.readline()
+            dagit = string.rsplit()
+            Mrate[i, t] = float(dagit[1])
+        f.readline(); f.readline()
+    Mratenew = np.reshape(Mrate, dim, order='F')
+    return Mratenew
+
+def make_colormap(seq):
+    """Return a LinearSegmentedColormap
+    seq: a sequence of floats and RGB-tuples. The floats should be increasing
+    and in the interval (0,1).
+    """
+    seq = [(None,) * 3, 0.0] + list(seq) + [1.0, (None,) * 3]
+    cdict = {'red': [], 'green': [], 'blue': []}
+    for i, item in enumerate(seq):
+        if isinstance(item, float):
+            r1, g1, b1 = seq[i - 1]
+            r2, g2, b2 = seq[i + 1]
+            cdict['red'].append([item, r1, r2])
+            cdict['green'].append([item, g1, g2])
+            cdict['blue'].append([item, b1, b2])
+    return mcolors.LinearSegmentedColormap('CustomMap', cdict)
+
+def readhypo(filename):
+    x = np.genfromtxt(filename,usecols=0)
+    y = np.genfromtxt(filename,usecols=1)
+    return x,y
+
+def read_subsources (filename):
+
+    f = open (filename, 'r')
+    print 'Reading the file', filename
+    print '...'
+    xgrid     = np.genfromtxt(filename, usecols=0)
+    ygrid     = np.genfromtxt(filename, usecols=1)
+    ssources  = np.genfromtxt(filename, usecols=2)
+    return xgrid, ygrid, ssources
+
+
+def readfileslip (NSR, filename):
+    xgrid = np.zeros((NSR))
+    ygrid = np.zeros((NSR))
+    slip  = np.zeros((NSR))
+    f = open (filename, 'r')
+    print 'Reading the file', filename
+    print '...'
+    for i in np.arange(NSR):
+        string   = f.readline()
+        degerler = [float(j) for j in string.split()]
+        xgrid[i] = degerler[0]
+        ygrid[i] = degerler[1]
+        slip [i] = degerler[2]
+    return xgrid, ygrid, slip
+
+def plotslip(nrows,ncols,LF,WF,slip,kaydet,figname,nucx,nucy):
+    print 'Plotting max-slip distribution'; print;
+    # Making a colormap
+    c    = mcolors.ColorConverter().to_rgb
+    cc = ['#ffffff', '#dfe6ff', '#a5b5da', '#516b8e', '#c5ce9b',
+          '#fcab6c', '#f50000']
+    cc1 = np.linspace(0,1,6)
+    cmap = make_colormap([c(cc[0]), c(cc[1]), cc1[0],
+                          c(cc[1]), c(cc[2]), cc1[1],
+                          c(cc[2]), c(cc[3]), cc1[2],
+                          c(cc[3]), c(cc[4]), cc1[3],
+                          c(cc[4]), c(cc[5]), cc1[4],
+                          c(cc[5]), c(cc[6]), cc1[5],
+                          c(cc[6])])
+    sns.set_style('whitegrid')
+    fig = p.figure(figsize=(18,10))
+    p.subplots_adjust(hspace=0.35)
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('Along strike [km]', fontsize=17)
+    ax.set_ylabel('Along up-dip [km]', fontsize=17)
+    
+    ax.set_xlim([0,1.1*LF])
+    ax.set_ylim([-0.2,1.1*WF])
+    vmin = slip.min()
+    vmax = slip.max()
+    print 'min and max of slip:'
+    print vmin,vmax; print;
+    grid = slip.reshape((nrows, ncols))
+    im = plt.imshow(grid, extent=(0.0,1.1*LF,0.0,1.1*WF), interpolation='bilinear', cmap=cmap, origin='lower')
+    formatter = LogFormatter(10, labelOnlyBase=False)
+    cb = fig.colorbar(im, shrink=0.5, aspect=10, pad=0.01, ticks=np.arange(vmin,vmax,1), format=formatter)
+    cb.set_label('Slip [m]', labelpad=20, y=0.5, rotation=90, fontsize=17)
+    p.setp(cb.ax.yaxis.get_ticklabels(), fontsize=16)
+    ax.plot(nucx,nucy, marker='*', color='red',markersize=20)
+    plt.xticks(fontsize=17)
+    plt.yticks(fontsize=17)
+    topname  = 'Max slip [m] = '+ '% .4f' % max(slip)
+    plt.title(topname+'\n', fontsize=20)
+    if kaydet:
+        fig.savefig(figname, dpi=300)
+    else:
+        plt.show()
+
+def plotsubsources(x,y,radius,LF,WF,kaydet,figname,nucx,nucy):
+
+    print 'Plotting sub-sources'; print;
+    
+    # Making a colormap
+    c    = mcolors.ColorConverter().to_rgb
+    cc = ['#ffffff', '#dfe6ff', '#a5b5da', '#516b8e', '#c5ce9b',
+          '#fcab6c', '#f50000']
+    
+    #cc1 = np.logspace(np.log10(0.25),np.log10(0.95),6)
+    cc1 = np.linspace(0,1,6)
+    
+    cmap = make_colormap([c(cc[0]), c(cc[1]), cc1[0], 
+                          c(cc[1]), c(cc[2]), cc1[1],
+                          c(cc[2]), c(cc[3]), cc1[2],
+                          c(cc[3]), c(cc[4]), cc1[3],
+                          c(cc[4]), c(cc[5]), cc1[4],
+                          c(cc[5]), c(cc[6]), cc1[5],
+                          c(cc[6])])
+    
+    # Figure parameters
+    sns.set_style('whitegrid')
+    fig = p.figure(figsize=(18,10))
+    p.subplots_adjust(hspace=0.35)
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('Along strike [km]', fontsize=17)
+    ax.set_ylabel('Along up-dip [km]', fontsize=17)
+    ax.set_xlim([0,1.1*LF])
+    ax.set_ylim([-0.2,1.1*WF])
+    i = 0
+    for xx,yy in zip(x,y):
+        circ = Circle((xx,yy),radius[i], fill=False, lw=1)
+        ax.add_patch(circ)	   
+        i = i+1
+    Nsub = len(radius)
+    topname  = 'Total number of subsources = '+ '%d' % Nsub
+    plt.title(topname+'\n', fontsize=20)
+    ax.plot(nucx,nucy, marker='*', color='red',markersize=20)
+    plt.xticks(fontsize=17)
+    plt.yticks(fontsize=17)
+    if kaydet:
+        fig.savefig(figname, dpi=300)
+    else:
+        plt.show()
+
+def plotgrid(nrows,ncols,LF,WF,kaydet,figname,hypox,hypoy,x,y):
+
+    print 'Creating the 2D grid...'
+    # Making a colormap
+    c    = mcolors.ColorConverter().to_rgb
+    cc = ['#ffffff', '#dfe6ff', '#a5b5da', '#516b8e', '#c5ce9b',
+          '#fcab6c', '#f50000']
+    #cc1 = np.logspace(np.log10(0.25),np.log10(0.95),6)
+    cc1 = np.linspace(0,1,6)
+    cmap = make_colormap([c(cc[0]), c(cc[1]), cc1[0], 
+                          c(cc[1]), c(cc[2]), cc1[1],
+                          c(cc[2]), c(cc[3]), cc1[2],
+                          c(cc[3]), c(cc[4]), cc1[3],
+                          c(cc[4]), c(cc[5]), cc1[4],
+                          c(cc[5]), c(cc[6]), cc1[5],
+                          c(cc[6])])
+    # Figure parameters
+    sns.set_style('whitegrid')
+    fig = p.figure(figsize=(18,10))
+    p.subplots_adjust(hspace=0.35)
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('Along strike [km]', fontsize=17)
+    ax.set_ylabel('Along up-dip [km]', fontsize=17)
+    ax.set_xlim([0,LF])
+    ax.set_ylim([-0.2,WF])
+    plt.scatter(x,y,s=2,c='gray')
+    ax.plot(hypox,hypoy, marker='*', color='red', markersize=25)
+    plt.xticks(fontsize=17)
+    plt.yticks(fontsize=17)
+    topname  = '2D grid for RIK model'
+    plt.title(topname+'\n', fontsize=20)
+    if kaydet:
+        fig.savefig(figname, dpi=300)
+    else:
+        plt.show()
---
+++ pysem/rikpp/rik_slip2moment.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""
+Main file to convert RIK output into SEM3D input
+"""
+
+#=======================================================================
+# Required modules
+#=======================================================================
+import os
+import numpy as np
+import h5py
+from matplotlib import pyplot as plt
+import seaborn as sns
+import rik_pp_lib as rp
+
+#=======================================================================
+# General informations
+#=======================================================================
+__author__ = "Filippo Gatti"
+__copyright__ = "Copyright 2018, MSSMat UMR CNRS 8579 - CentraleSupélec"
+__credits__ = ["Filippo Gatti"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Filippo Gatti"
+__email__ = "filippo.gatti@centralesupelec.fr"
+__status__ = "Beta"
+
+class SEM_source(object):
+    def __init__(self,e=0.,n=0.,z=0.):
+        self.e = e
+        self.n = n
+        self.z = z
+    def __call__(self,e=0.,n=0.,z=0.):
+        self.e = e
+        self.n = n
+        self.z = z
+    def get_hypo(self):
+        return np.array([self.e,self.n,self.z])
+
+class RIK_source(object):
+    def __init__(self,x=0.,y=0.,z=0.,hypfile=None):
+        self.x = x
+        self.y = y
+        self.z = z
+        if hypfile:
+            self.read_hypo(hypfile)
+    def __call__(self,x=0.,y=0.,z=0.):
+        self.x = x
+        self.y = y
+        self.z = z
+        if hypfile:
+            self.read_hypo(hypfile)
+    def read_hypo(self,hypfile):
+        self.x, self.y = rp.readhypo(hypfile)
+    def get_hypo(self):
+        return np.array([self.x,self.y,self.z])
+
+if __name__=='__main__':
+    fld = '/home/filippo/Data/Filippo/ares/workdir/RIK/napa_2014/results'
+    plot_figure     = True
+    figurename = 'napa_2014_moment_vs_time.png'
+
+    # Saving point-coordinates in file ...?
+    coord_file_name = 'Coordinates_SEM3D_extended_fault_points.dat'
+
+    # Input files 
+    RIK_slipfile = os.path.join(fld, 'slipdistribution.dat')
+    mrfile = os.path.join(fld, 'MomentRate.dat')
+    hypfile = os.path.join(fld,'nucleationpoint.dat')
+
+    NT = 480
+    dt = 0.025
+    T_total = (NT-1)*dt
+    time = np.linspace(0.0, T_total, NT)
+    # Nombre de points de grille (toute la grille si different 
+    # que strong-motion generation area)
+    NL = 150 #210
+    NW = 100 #170
+
+    # Les angles 
+    strike = 38#35.0
+    dip    = 63#35.0
+    rake   = 172#-172.0
+    # Les angles en radian
+    aS    = np.pi/180.0* strike
+    aR    = np.pi/180.0* rake
+    aD    = np.pi/180.0* dip
+    # Rotation matrix
+    print 'Rotation matrix: '
+    MatMesh = rp.get_rotation_tensor(aS, aD)
+
+    src_sem = SEM_source(e=2.2754e+06,n=4.2421e+06,z=-10.0e+3)
+    # Lire les coordonnees de l'hypocentre (point de nucleation)
+    src_rik = RIK_source(hypfile=hypfile)
+    src_rik(z=10.e+3)
+    # On suppose que l'hypocentre est  (en metres)
+    RIK_hypocenter = src_rik.get_hypo()  # Y_RIK, X_RIK, DEPTH(DOWNWARD)
+    SEM_hypocenter = src_sem.get_hypo()  # EST,   NORD,  UPWARD
+
+    MR = rp.read_moment_rate_RIK (mrfile, (int(NL), int(NW), int(NT)))
+
+    # Creation des fichiers d'entree pour SEM
+    kinefile_out  = h5py.File('napa2014_kine.hdf5', 'w')
+    slipfile_out  = h5py.File('napa2014_moment.hdf5','w')
+    # Attribuer les proprietes temporelles
+    kinefile_out.attrs['Nt'] = NT
+    kinefile_out.attrs['dt'] = time[1]-time[0]
+    kinefile_out.attrs['Ns'] = NL 
+    kinefile_out.attrs['Nd'] = NW
+
+    # Vecteurs du normale et du slip
+    rp.vecteurs(aD,aS,aR,kinefile_out)
+
+    # Coordonnees des points dans le repere SEM3D
+    xgrid = kinefile_out.create_dataset('x', (NL, NW), chunks=(1, 1))
+    ygrid = kinefile_out.create_dataset('y', (NL, NW), chunks=(1, 1))
+    depth = kinefile_out.create_dataset('z', (NL, NW), chunks=(1, 1))
+
+    xgrid[:,:] = 0.0
+    ygrid[:,:] = 0.0
+    depth[:,:] = 0.0
+
+    # x,y koordinatlari (RIK) - metre
+    print '*********'
+    print 'Hypocenter of RIK model:'
+    print RIK_hypocenter[0], RIK_hypocenter[1], RIK_hypocenter[2]
+    print 'Hypocenter of SEM model:'
+    print SEM_hypocenter[0], SEM_hypocenter[1], SEM_hypocenter[2]
+    # RIK model - Les coordonnees de x et y
+    RIK_xcoord = np.genfromtxt(RIK_slipfile, usecols=1)  # Y_RIK
+    RIK_ycoord = np.genfromtxt(RIK_slipfile, usecols=0)  # X_RIK
+
+    # Creer le matrice hdf5 pour le temps
+    slipfile_out.create_dataset('time', data=time)
+
+    # Creer le matrice hdf5 pour le moment
+    Moment = slipfile_out.create_dataset('moment', (NL, NW, NT), chunks=(1, 1, NT))
+
+    # Integration pour calculer le moment
+    print 'INTEGRATION'
+    n = 0
+    for i in range(0, NL):
+        for j in range(0, NW):
+            n = n+ 1
+            Moment[i,j,:] = rp.trapezoid(NT, dt, MR[i,j,:])
+            print NT, dt, '  --->  Point ', n
+
+    print 'Total point number in fault plane: ', NL*NW
+    n = 0
+    for j in np.arange(NW):
+        for i in np.arange(NL):
+            # en kilometres
+            if (NL*NW > 1):
+                xgrid[i,j] = RIK_xcoord[n]
+                ygrid[i,j] = RIK_ycoord[n]
+            else:
+                print 'ATTENTION: CHANGE THIS FOR MODELS WITH MORE THAN 1 POINT'
+                xgrid[i,j] = RIK_xcoord
+                ygrid[i,j] = RIK_ycoord
+            depth[i,j] = (RIK_hypocenter[2]/1e3+ np.sin(aD)* (RIK_hypocenter[0]/1e3-xgrid[i,j]))
+
+            # en metres
+            xgrid[i,j] = xgrid[i,j]* 1e3
+            ygrid[i,j] = ygrid[i,j]* 1e3
+            depth[i,j] = depth[i,j]* 1e3
+
+            n = n+1
+    # ATTENTION: Once rotate edip sonra farkina bakiyorum
+    fark  = np.dot(MatMesh,RIK_hypocenter)
+    fark  = (SEM_hypocenter-fark)
+
+    print 'fark', fark
+    # Opening file to save SEM3D coordinates of grid points
+    coord_file = open (coord_file_name, 'w')
+    # Burada rotation yapiyorum
+    n = 0
+    for j in np.arange(NW):
+        for i in np.arange(NL):
+            n = n+ 1
+            print 'Point ',n,	
+            # Rotasyon uyguluyorum
+            coord = np.array([xgrid[i,j], ygrid[i,j], depth[i,j]])
+            dum   = np.dot(MatMesh,coord)
+
+            # Aradaki farki ekliyorum; boylelikle translate ediyor
+            xgrid[i,j] = dum[0]+ fark[0]
+            ygrid[i,j] = dum[1]+ fark[1]
+            depth[i,j] = dum[2]+ fark[2]
+
+            # Writing out the cordinates
+            coord_file.write('%s \t %s \t %s \t %s \n'\
+                    %  (str(n), str(xgrid[i,j]), str(ygrid[i,j]), str(depth[i,j])))
+            print '***'
+    coord_file.close()
+
+
+    if plot_figure:
+        ### PLOTTING ###
+        fig = plt.figure(figsize=(12,10))
+        sns.set_style('whitegrid')
+        ax  = fig.add_subplot(111)
+
+        # ax.set_xlim([0,1])
+        # #
+        for i in range(0, NL):
+            for j in range(0, NW):
+                ax.plot(time, Moment[i,j,:], label='Point '+str(i+1),color='Gray')
+        # #
+        ax.set_xlabel('Time [s]'    ,fontsize=20)
+        ax.set_ylabel('Moment [Nm]' ,fontsize=20)
+        # ax.legend()
+        plt.show()
+        fig.savefig(figurename,dpi=300)
+
+    # Fermeture des fichiers hdf5
+    kinefile_out.close()
+    slipfile_out.close()
