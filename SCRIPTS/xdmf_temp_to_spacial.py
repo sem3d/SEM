0efffd98e147e343118e166e5d7dd12fdb838685
Ludovic Aubry <ludovic.aubry@cea.fr>
2022-04-01 11:40:28 +0200

    maj script conversion spatial->temporal

--- SCRIPTS/xdmf_temp_to_spacial.py
+++ SCRIPTS/xdmf_temp_to_spacial.py
@@ -1,4 +1,6 @@
+from __future__ import print_function
 import sys
+
 
 from xml.etree import ElementTree as ET
 from xml.etree import ElementInclude as EI
@@ -21,39 +23,66 @@ newtop=ET.Element("Grid", GridType= 'Collection', CollectionType='Temporal', Nam
 
 temporal_grids = [ sp.getchildren()[0].getchildren()[0] for sp in spatialgrids ]
 
-
 common = {}
 
-ts = 0
-for sg in temporal_grids:
-    if sg[0].tag == "DataItem":
-        name = sg[0].attrib["Name"]
-        common[name] = sg
-        continue
-    time = sg[0].find("Time")
-    if time is None:
-        toto
-    spgrid = newtop.makeelement("Grid", { "GridType" : 'Collection', "CollectionType" : 'Spatial', "Name" : 'timestep.%d' % ts })
-    spgrid.append(time)
-    for i, subgrid in enumerate(sg):
-        if Nmax>0 and i>=Nmax:
-            continue
-        ng = spgrid.makeelement("Grid", subgrid.attrib)
-        for item in subgrid.getchildren():
-            if item.tag == "Time":
-                continue
-            if item.tag == "Attribute":
-                name = item.attrib["Name"]
-                if name in common:
-                    item.getchildren()[0] = common[name][i]
-            if item is None:
-                continue
-            ng.append(item)
-        spgrid.append(ng)
-    newtop.append(spgrid)
-    ts = ts+1
+grids = {}
 
+for temp_grid in temporal_grids:
+    space_name = temp_grid.attrib["Name"]
+    for mesh_grid in temp_grid.getchildren():
+        mesh_name = mesh_grid.attrib["Name"]
+        for i,el in enumerate(mesh_grid.getchildren()):
+            if el.tag == "Time":
+                break
+        timeval = float(el.attrib["Value"].strip())
+        del mesh_grid[i]
+        _, gridlist = grids.setdefault(timeval,(el, []))
+        gridlist.append(mesh_grid)
+
+for ts, tm in enumerate(sorted(grids.keys())):
+    timeel, gridlist = grids[tm]
+    gridlist.sort(key=lambda g:g.attrib["Name"])
+    spgrid = newtop.makeelement("Grid", { "GridType" : 'Collection', "CollectionType" : 'Spatial', "Name" : 'timestep.%d' % ts })
+    spgrid.append(timeel)
+    spgrid.tail="\n"
+    for i,g in enumerate(gridlist):
+        if i<3:
+            continue
+        spgrid.append(g)
+        if Nmax>=0 and i>=Nmax:
+            break
+    newtop.append(spgrid)
+
+## ts = 0
+## for sg in temporal_grids:
+##     if sg[0].tag == "DataItem":
+##         name = sg[0].attrib["Name"]
+##         common[name] = sg
+##         continue
+##     time = sg[0].find("Time")
+##     if time is None:
+##         toto
+##     spgrid = newtop.makeelement("Grid", { "GridType" : 'Collection', "CollectionType" : 'Spatial', "Name" : 'timestep.%d' % ts })
+##     spgrid.append(time)
+##     for i, subgrid in enumerate(sg):
+##         if Nmax>0 and i>=Nmax:
+##             continue
+##         ng = spgrid.makeelement("Grid", subgrid.attrib)
+##         for item in subgrid.getchildren():
+##             if item.tag == "Time":
+##                 continue
+##             if item.tag == "Attribute":
+##                 name = item.attrib["Name"]
+##                 if name in common:
+##                     item.getchildren()[0] = common[name][i]
+##             if item is None:
+##                 continue
+##             ng.append(item)
+##         spgrid.append(ng)
+##     newtop.append(spgrid)
+##     ts = ts+1
+## 
 domain.getchildren()[0] = newtop
 
-print ET.tostring(root)
+print(ET.tostring(root))
 
